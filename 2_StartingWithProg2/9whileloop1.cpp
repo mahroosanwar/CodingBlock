27af8d215a70fc5d46ae9017571464e6456c6e89
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-01-27 05:40:53 +0530

    Create 9whileloop1.cpp

---
+++ 2_StartingWithProg2/9whileloop1.cpp
@@ -0,0 +1,18 @@
+// While loop starting
+// print 1 to 10 numbers
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;              //Initialization
+    while (i <= 10)         //Condition
+    {
+        cout << i << " ";   //task
+
+        i = i + 1 ;         //Updation
+
+    }
+    return 0;
+}
