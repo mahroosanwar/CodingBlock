0f51f6c536c5ad0a15ec00907287b8b52833e153
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-02-10 18:43:48 +0530

    Update 3Lower_UpperBound.cpp

--- 10_Algorithm_STL/3Lower_UpperBound.cpp
+++ 10_Algorithm_STL/3Lower_UpperBound.cpp
@@ -5,19 +5,26 @@
 using namespace std;
 
 int main(){
-    int a[] = {10, 20, 40, 40, 50};
+    int a[] = {10, 20, 40, 40, 40, 50, 60, 100};
     int n = sizeof(a)/sizeof(int);
 
     int key;
     cin >> key;
     
-    //lower bound
+    //lower bound (first occurance of key)
 
-    int*ans = lower_bound(a, a+n, key);         //return address of >= key
-
+    int*ans = lower_bound(a, a+n, key);     //return address of >= key
 
     int indx = ans-a;
-    cout << "Key found at: " << indx << endl;
-    
+    cout << "key: " << *ans << endl;
+    cout << "Key found at(first occurance): " << indx << endl;
+
+    //Upper bound (last occurance of Key)
+    int *x = upper_bound(a, a+n, key);      //return address of >key
+    int index = x-a-1;
+    cout << "key: " << *x << endl;
+    cout << "key found at(last occurance): " << index << endl;      
+
+    cout << "no of occurance of key " << x-ans << endl;
     return 0;
 }
