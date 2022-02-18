40c33dccc62f3b3f46da38eb241cf1bea50b22eb
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-02-18 23:35:04 +0530

    Create 1BitwiseOperatorBasic.cpp

---
+++ 11_Bitmanipulation/1BitwiseOperatorBasic.cpp
@@ -0,0 +1,11 @@
+// Bitwise Operator basic
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << (5&7) << endl; //5
+    cout << (5|7) << endl; //7
+    cout << ~5 << endl; //-6 bcz take 2's of the number gives the correct answer
+    cout << (5^7) << endl; //2
+}
