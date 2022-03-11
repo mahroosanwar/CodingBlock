05781c8b784d3bc359062bd359289910952e50e9
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-03-11 23:14:36 +0530

    Create 1SpaceTimeComplexityIntroduction.cpp

---
+++ 19_SpaceTimeComplexityAnalysis/1SpaceTimeComplexityIntroduction.cpp
@@ -0,0 +1,50 @@
+//  Space Time Complexity Introduction
+//  Comparison b/w bubble and merge sort(we use inbuilt sort function which also run in  nlogn complexity )
+
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+using namespace std;
+
+void bubble_sort(int *a, int n){
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-1; j++){
+            if(a[j] > a[j+1]){
+                swap(a[j], a[j+1]);
+            }
+        }
+    } 
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int a[100000];
+
+    // time_t start = clock(); 
+
+    //create a reverse sorted array
+    for(int i = 0; i<n; i++){
+        a[i] = n-i;
+    }
+
+    time_t start = clock();
+    bubble_sort(a, n);
+    time_t end = clock();
+    
+    cout << "Bubble sort time: " << end  - start << endl;
+    
+
+    for(int i = 0; i<n; i++){
+        a[i] = n - i;
+    }
+    //inbuilt sort ( insted of merge sort)
+    start = clock();
+    sort(a, a+n);
+    end = clock();
+
+    cout << "Merge Sort time: " << end - start << endl;    
+
+    return 0;
+}
+
