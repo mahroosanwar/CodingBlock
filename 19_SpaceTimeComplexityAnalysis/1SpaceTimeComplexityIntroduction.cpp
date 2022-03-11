b34ae89080cb6a8b3498c45686e36c024da3739d
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-03-11 23:33:10 +0530

    Update 1SpaceTimeComplexityIntroduction.cpp

--- 19_SpaceTimeComplexityAnalysis/1SpaceTimeComplexityIntroduction.cpp
+++ 19_SpaceTimeComplexityAnalysis/1SpaceTimeComplexityIntroduction.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 void bubble_sort(int *a, int n){
     for(int i=0; i<n-1; i++){
-        for(int j=0; j<n-1; j++){
+        for(int j=0; j<n-i-1; j++){
             if(a[j] > a[j+1]){
                 swap(a[j], a[j+1]);
             }
