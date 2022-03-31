ca3d14a203aa86afc7ace5e9f02675b3e9a2b54d
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-03-31 22:26:27 +0530

    Add files via upload

---
+++ 23_LinkedLists/ChallengesLinkedLists/10LinkedList_ReverseRecursive_Data.cpp
@@ -0,0 +1,77 @@
+//Linked List - Reverse a Linked List (Recursive, Data)
+
+#include <iostream>
+using namespace std;
+
+class node{
+public:
+    int data;
+    node* next;
+
+    //constructor
+    node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node*&head, int data){
+
+    if(head == NULL){
+        head = new node(data);
+        return;
+    }
+
+    node*temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = new node(data);
+    return;
+}
+
+void print(node*head){
+
+    while(head != NULL){
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+node* recReverse(node*head){
+
+    //base case
+    if(head == NULL || head->next == NULL) {
+        return head;
+    }
+
+    //rec case
+
+    node* smallHead = recReverse(head->next);
+
+    // node* temp = head->next;        //by making this; optimize
+    // temp->next = head;
+    head->next->next = head;
+    head->next = NULL;
+    return smallHead;
+}
+
+int main(){
+    node* head = NULL;
+    
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        int data;
+        cin >> data;
+
+        insertAtTail(head, data);
+    }
+
+    node* smallHead = recReverse(head);
+    print(smallHead);
+
+    return 0;
+}
