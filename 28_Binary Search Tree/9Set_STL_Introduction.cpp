a1b94d4d2aa2447981d15b78f641fe1f78daa1d0
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-04-19 20:31:57 +0530

    Add files via upload

---
+++ 28_Binary Search Tree/9Set_STL_Introduction.cpp
@@ -0,0 +1,25 @@
+//Set STL Introduction
+
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 20, 11, 9, 8, 11, 10};
+    int n = sizeof(arr)/sizeof(int);
+
+    set<int> s;
+    for(int i=0; i<n; i++) {
+        s.insert(arr[i]);
+    }
+
+    s.erase(10);
+    auto it = s.find(11);
+    s.erase(it);
+
+    //print all elements 
+    for(set<int> :: iterator it = s.begin(); it!=s.end(); it++) {
+        cout << *(it) << " ";
+    }
+    return 0;
+}
