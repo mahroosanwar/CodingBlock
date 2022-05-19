59928fb3662143d307e70a480eb498cfce623d6f
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-05-19 23:55:51 +0530

    Add files via upload

---
+++ 30_Hashing - Hashtable/Challenges Hashing/6Tree_bottom_view.cpp
@@ -0,0 +1,97 @@
+//Tree bottom view
+
+#include<iostream>
+#include<map>
+#include<queue>
+#include<vector>
+using namespace std;
+
+class node{
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d) {
+        data = d;
+        left = right = NULL;
+    }
+};
+
+node *buildLevelOrder() {
+    int d;
+    cin >> d;
+
+    queue<node *> q;
+    node *root = new node(d);
+    q.push(root);
+
+    while(!q.empty()) {
+        node *f = q.front();
+        q.pop();
+
+        int c1, c2;
+        cin >> c1 >> c2;
+
+        if(c1 != -1) {
+            f->left = new node(c1);
+            q.push(f->left);
+        }
+
+        if(c2 != -1) {
+            f->right = new node(c2);
+            q.push(f->right);
+        }
+    }
+    return root;
+}
+
+vector<int> TreeBottomView(node *root) {
+    vector<int> ans;
+
+    //base case
+    if(root == NULL) {
+        return ans;
+    }
+
+    map<int, int> m;    //line(horizontal distance from root) and data      
+    queue<pair<node*, int> > q;
+    q.push({root, 0});
+
+    while(!q.empty()) {
+        auto it = q.front();
+        q.pop();
+
+        node* temp = it.first;
+        int line = it.second;
+
+        m[line] = temp->data;
+
+        if(temp->left != NULL) {
+            q.push({temp->left, line - 1});
+        }
+
+        if(temp->right != NULL) {
+            q.push({temp->right, line + 1});
+        }
+    }
+
+    for(auto it : m) {
+        ans.push_back(it.second);
+    }
+
+    return ans;
+}
+
+int main() {
+
+    node* root = buildLevelOrder();
+
+    vector<int> v = TreeBottomView(root);
+
+    for(auto i : v) {
+        cout << i << " ";
+    }    
+
+    return 0;
+}
