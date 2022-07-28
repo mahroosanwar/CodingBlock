97aad7b00f34ec9d762a0d49ccf5641765ffb8b5
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-07-28 16:27:49 +0530

    Add files via upload

---
+++ 34_Number Theory Basics/9DivisibleSubarrays.cpp
@@ -0,0 +1,43 @@
+//9: Divisible Subarrays
+
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+#define ll long
+
+ll a[1000005], pre[1000005];
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+
+        memset(pre, 0, sizeof(pre));
+
+        pre[0] = 1;     //freq of null subarray is 1 so freq of 0 is 1
+
+        int sum = 0;
+        for(int i = 0; i<n; i++) {
+            
+            cin >> a[i];
+            sum += a[i];
+            sum %= n;
+            sum = (sum + n) % n;
+            pre[sum]++;
+        }
+
+        ll ans = 0;
+        for(int i=0; i<n; i++) {
+            ll m = pre[i];
+            ans += (m) * (m-1)/2;
+        }
+        
+        cout << ans << endl;
+
+    }
+}
