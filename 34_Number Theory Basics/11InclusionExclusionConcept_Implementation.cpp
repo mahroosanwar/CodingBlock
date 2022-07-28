cb0ef635e61bf81c08389a3ceca68a34c9df352a
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-07-28 17:39:54 +0530

    Add files via upload

---
+++ 34_Number Theory Basics/11InclusionExclusionConcept_Implementation.cpp
@@ -0,0 +1,46 @@
+//11: Inclusion Exclusion Concept + Implementation
+
+#include<iostream>
+using namespace std;
+
+#define ll long long
+
+int main() {
+
+    ll t;
+    cin >> t;
+
+    ll primes[] = {2, 3, 5, 7, 11, 13, 17, 19};
+
+    while(t--) {
+
+        ll n;
+        cin >> n;
+
+        ll subsets = (1<<8) - 1;
+        ll ans = 0;
+        for(ll i = 1; i<=subsets; i++) {
+
+            ll denom = 1ll;
+
+            ll setBits = __builtin_popcount(i);  //inbuilt method :-  gives the no of set bits i has 
+
+            for(ll j = 0; j<=7; j++) {
+                 
+                if(i&(1<<j)) {
+                   denom = denom * primes[j];
+                }
+            }
+
+            if(setBits&1) {
+                ans += n/denom;
+            }
+            else {
+                ans -= n/denom;
+            }
+        }
+        cout << ans  << endl;
+    }
+
+    return 0;
+}
