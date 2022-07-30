e2ff897eaf8998666b09bb504bcbcfc1ebdc7a4a
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-07-30 15:56:33 +0530

    Update 4BottomUpFibonacci_DP_SpaceOptimisation.cpp

--- 35_Dynamic Programming/4BottomUpFibonacci_DP_SpaceOptimisation.cpp
+++ 35_Dynamic Programming/4BottomUpFibonacci_DP_SpaceOptimisation.cpp
@@ -14,11 +14,32 @@ int fibBU(int n) {
     return dp[n];
 }
 
+//bottom up approach with space optimization
+int fibBU_space_Opt(int n) {
+
+    //base case
+    if(n == 0 || n == 1) {
+        return n;
+    }
+
+    int a = 0;
+    int b = 1;
+    int c;
+
+    for(int i=2; i<=n; i++) {
+        c = a + b;
+        a = b;
+        b = c; 
+    }
+
+    return c; 
+}
+
 int main() {
 
     int n;
     cin >> n;
 
-    cout << fibBU(n);
+    cout << fibBU_space_Opt(n);
     return 0;
 }
