168efaa859eae2a65f37590f5dff98829628acb8
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 14:41:13 +0530

    Update 5HollowRhombus.cpp

--- 2_StartingWithProg2/ChallangesGSWP2/5HollowRhombus.cpp
+++ 2_StartingWithProg2/ChallangesGSWP2/5HollowRhombus.cpp
@@ -48,3 +48,25 @@ int main()
     }
     return 0;
 }
+
+// Given number of rows N, you have to print a Hollow Rhombus. See the output for corresponding given input.
+
+// Input Format
+// Single integer N.
+
+// Constraints
+// N <= 20
+
+// Output Format
+// Print pattern.
+
+// Sample Input
+// 5
+// Sample Output
+//     *****
+//    *   *
+//   *   *
+//  *   *
+// *****
+// Explanation
+// For any input N. First line contains 4 space and then 5 {*} and then the second line contains according to the output format.
