60a865feae55cdd813273c8cb4ff5f531ed7508c
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:13:53 +0530

    Update 6RecursionPattern_1Triangle.cpp

--- 13_DivingIntoRecursion/ChallengesDivingIntoRecursion/6RecursionPattern_1Triangle.cpp
+++ 13_DivingIntoRecursion/ChallengesDivingIntoRecursion/6RecursionPattern_1Triangle.cpp
@@ -26,3 +26,33 @@ int main() {
 	triangle2(n,1);
 	return 0;
 }
+
+// Take as input N, a number. Print the following pattern (for N =4)
+
+// *
+
+// * *
+
+// * * *
+
+// * * * *
+
+// * * * * *
+
+// Input Format
+// Enter the value of N
+
+// Constraints
+// None
+
+// Output Format
+// Tab separated (*)
+
+// Sample Input
+// 5
+// Sample Output
+// *
+// *	*
+// *	*	*
+// *	*	*	*
+// *	*	*	*	*
