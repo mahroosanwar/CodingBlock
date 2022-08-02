7e7b8e860f47b632ff10ff2a9285f137f19528e3
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 16:06:12 +0530

    Update 7MaxCircularSum.cpp

--- 5_Arrays/Challenges1DArray/7MaxCircularSum.cpp
+++ 5_Arrays/Challenges1DArray/7MaxCircularSum.cpp
@@ -61,6 +61,32 @@ int main(){
     return 0;
 }
 
+//Question: 
+// You are provided n numbers (both +ve and -ve). Numbers are arranged in a circular form. You need to find the maximum sum of consecutive numbers.
+
+// Input Format
+// First line contains integer t which is number of test case.
+// For each test case, it contains an integer n which is the size of array and next line contains n space separated integers denoting the elements of the array.
+
+// Constraints
+// 1<=t<=100
+// 1<=n<=1000
+// |Ai| <= 10000
+
+// Output Format
+// Print the maximum circular sum for each testcase in a new line.
+
+// Sample Input
+// 1
+// 7
+// 8 -8 9 -9 10 -11 12
+// Sample Output
+// 22
+// Explanation
+// Maximum Circular Sum = 22 (12 + 8 - 8 + 9 - 9 + 10)
+
+
+//Approach:
 // For maximum circular sub array sum you need to consider the maximum of these 2 cases:
 
 // The maxm sum subarray is obtained in non circular fashion as in normal Kadane’s algorithm.
