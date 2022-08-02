d2aa4dcc3561a64eed18e52f261ac55f84ac3a2d
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:21:52 +0530

    Update 4DuplicateCharacterFormatting_Rec.cpp

--- 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/4DuplicateCharacterFormatting_Rec.cpp
+++ 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/4DuplicateCharacterFormatting_Rec.cpp
@@ -44,3 +44,23 @@ int main(){
 
     return 0;
 }
+
+
+
+// Take as input str, a string. Write a recursive function which returns a new string in which all duplicate consecutive characters are separated by a ‘ * ’. E.g. for “hello” return “hel*lo”. Print the value returned
+
+// Input Format
+// Enter a String
+
+// Constraints
+// 1<= Length of string <= 10^4
+
+// Output Format
+// Display the resulting string (i.e after inserting (*) b/w all the duplicate characters)
+
+// Sample Input
+// hello
+// Sample Output
+// hel*lo
+// Explanation
+// We insert a "*" between the two consecutive 'l' .
