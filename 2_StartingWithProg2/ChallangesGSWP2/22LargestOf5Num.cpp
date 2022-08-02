704f211a3be9e70e4563afedae597fc1c7e23200
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:10:25 +0530

    Update 22LargestOf5Num.cpp

--- 2_StartingWithProg2/ChallangesGSWP2/22LargestOf5Num.cpp
+++ 2_StartingWithProg2/ChallangesGSWP2/22LargestOf5Num.cpp
@@ -22,3 +22,17 @@ int main()
     
     return 0;
 }
+
+// Read as input 5 Numbers and print the largest out of them
+
+// Input Format
+// 5 Space Separated Numbers (both positive and negative)
+
+// Constraints
+// Output Format
+// Sample Input
+// 2 4 7 -2 3
+// Sample Output
+// 7
+// Explanation
+// In the given case 7 is largest among the given numbers.
