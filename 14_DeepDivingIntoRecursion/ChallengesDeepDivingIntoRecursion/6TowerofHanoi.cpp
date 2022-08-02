9f5115a271eb25fb1798f8a589bd8c8db7d0a947
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:23:25 +0530

    Update 6TowerofHanoi.cpp

--- 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/6TowerofHanoi.cpp
+++ 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/6TowerofHanoi.cpp
@@ -37,3 +37,32 @@ int main(){
 
     return 0;
 }
+
+// Google Tower of Hanoi.
+
+// Using a helper stick (peg), shift all rings from peg A to peg B using peg C. All rings are initally placed in ascending order, smallest being on top.
+
+// No bigger ring can be placed over a smaller ring.
+
+// a. Write a recursive function which prints the steps required to solve the tower of Hanoi problem for N discs.
+
+// b. Write a recursive function which returns number of steps required to solve the tower of Hanoi problem for N discs.
+
+// Let T1 be the Source Tower, T2 be the Destination Tower and T3 be the Auxillary Tower.
+
+// Input Format
+// Enter the number of discs
+
+// Constraints
+// None
+
+// Output Format
+// Display the steps required to solve the tower and also print the total number of steps required
+
+// Sample Input
+// 2
+// Sample Output
+// Move 1th disc from T1 to T3
+// Move 2th disc from T1 to T2
+// Move 1th disc from T3 to T2
+// 3
