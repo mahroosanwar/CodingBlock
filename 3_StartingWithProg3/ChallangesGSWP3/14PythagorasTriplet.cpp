12d01dd9937f016c9f6a5f8274386f7412348f24
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:47:36 +0530

    Update 14PythagorasTriplet.cpp

--- 3_StartingWithProg3/ChallangesGSWP3/14PythagorasTriplet.cpp
+++ 3_StartingWithProg3/ChallangesGSWP3/14PythagorasTriplet.cpp
@@ -24,3 +24,20 @@ int main()
     return 0;
 
 }
+
+
+// Given a number N (denoting one of the legs of the triangle), Print its Pythagoras pair in increasing order if they exist. Otherwise, print "-1".
+
+// Input Format
+// A single integer N
+
+// Constraints
+// N <= 10^9
+
+// Output Format
+// Two numbers X and Y denoting the rest of the numbers of the Pythagorean triplet in increasing order.
+
+// Sample Input
+// 3
+// Sample Output
+// 4 5
