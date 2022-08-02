aa79f2d53d8319a72b58fa15a2ee91f7124966d9
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:43:40 +0530

    Update 10GCD.cpp

--- 3_StartingWithProg3/ChallangesGSWP3/10GCD.cpp
+++ 3_StartingWithProg3/ChallangesGSWP3/10GCD.cpp
@@ -32,3 +32,30 @@ int main()
     return 0;
 
 }
+
+
+// Take the following as input.
+
+// A number (N1)
+// A number (N2)
+// Write a function which returns the GCD of N1 and N2. Print the value returned.
+
+
+
+// Input Format
+// Two integers seperated by a new line.
+
+// Constraints
+// 0 < N1 < 1000000000
+// 0 < N2 < 1000000000
+
+// Output Format
+// Output a single integer which is the GCD of the given integers.
+
+// Sample Input
+// 16 
+// 24
+// Sample Output
+// 8
+// Explanation
+// The largest number that divides both N1 and N2 is called the GCD of N1 and N2.
