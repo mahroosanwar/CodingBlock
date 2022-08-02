54a7bd3e3ec83e0615ff185e96113542fcd6527e
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 16:27:24 +0530

    Update 7TransposeOfMatrix.cpp

--- 6_2DArray/Challenges2D_Array/7TransposeOfMatrix.cpp
+++ 6_2DArray/Challenges2D_Array/7TransposeOfMatrix.cpp
@@ -37,3 +37,30 @@ int main(){
     cout << endl;
     return 0;
 }
+
+
+
+// Given a square matrix, print its transpose.
+
+// Input Format
+// First line contains the N ,size of the square matrix. Next N lines contains N integers each denoting the elements of the matrix
+
+// Constraints
+// 1 <= N <= 10^3
+
+// Output Format
+// Print N lines each containing N elements. These are the elements of the new matrix.
+
+// Sample Input
+// 5
+// 1 46 4 60 100 
+// 28 52 97 80 59 
+// 6 33 62 42 12 
+// 57 31 56 89 47 
+// 1 50 73 53 99 
+// Sample Output
+// 1 28 6 57 1 
+// 46 52 33 31 50 
+// 4 97 62 56 73 
+// 60 80 42 89 53 
+// 100 59 12 47 99 
