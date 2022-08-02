6ddc1eba2cffa31afe9cec20b7e828ddb160b45d
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:25:08 +0530

    Update 7SubsetsRec.cpp

--- 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/7SubsetsRec.cpp
+++ 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/7SubsetsRec.cpp
@@ -45,3 +45,30 @@ int main() {
         cout<<endl;
     }
 }
+
+
+// Find all the subsets of a given array that sum to k.
+
+// Input Format
+// The first line contains an integer N , the size of the array. The next line conatins N integers. The next line contains an integer K.
+
+// Constraints
+// 0 < N <= 20
+
+// Output Format
+// Output all the subsets that sum to K. The output should be printed as follows :
+
+// 1. Sets with least no of elements should be printed first.
+// 2. For equal length, elements that appear later in the original set,should be printed first.
+// Sample Input
+// 5
+// 1 4 6 5 3
+// 10
+// Sample Output
+// 6 4
+// 3 6 1
+// 5 4 1
+// Explanation
+// 4 + 6 = 10
+// 1 + 4 + 5 = 10
+// 1 + 3 + 6 = 10
