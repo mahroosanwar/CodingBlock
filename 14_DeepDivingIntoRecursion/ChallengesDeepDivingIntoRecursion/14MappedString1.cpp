9fb1986c5a5bb9205a320dd186bc287d4c68c91f
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:31:44 +0530

    Update 14MappedString1.cpp

--- 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/14MappedString1.cpp
+++ 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/14MappedString1.cpp
@@ -41,3 +41,25 @@ int main(){
 
     return 0;
 }
+
+// We are given a hashmap which maps all the letters with number. Given 1 is mapped with A, 2 is mapped with B…..26 is mapped with Z. Given a number, you have to print all the possible strings.
+
+// Input Format
+// A single line contains a number.
+
+// Constraints
+// Number is less than 10^6
+
+// Output Format
+// Print all the possible strings in sorted order in different lines.
+
+// Sample Input
+// 123
+// Sample Output
+// ABC
+// AW
+// LC
+// Explanation
+// '1' '2' '3' = ABC
+// '1' '23' = AW
+// '12' '3' = LC
