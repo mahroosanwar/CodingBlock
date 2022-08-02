f075a3702dbb5c2b8a130552647849675b767ba7
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:37:40 +0530

    Update 3BinaryToDecimal.cpp

--- 3_StartingWithProg3/ChallangesGSWP3/3BinaryToDecimal.cpp
+++ 3_StartingWithProg3/ChallangesGSWP3/3BinaryToDecimal.cpp
@@ -18,3 +18,17 @@ int main()
     cout << num << endl;
     return 0;
 }
+
+// Take N (number in binary format). Write a function that converts it to decimal format and Print the value returned.
+
+// Input Format
+// Constraints
+// 0 < N <= 1000000000
+
+// Output Format
+// Sample Input
+// 101010
+// Sample Output
+// 42
+// Explanation
+// For binary number fedcba , Decimal number = f * 25 + e * 24 + d * 23 + …..+ a * 20.
