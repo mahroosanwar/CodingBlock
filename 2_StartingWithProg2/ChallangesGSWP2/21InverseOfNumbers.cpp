d1551c824f5d1d1ad6da029e59269ce405c5ee4d
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:09:36 +0530

    Update 21InverseOfNumbers.cpp

--- 2_StartingWithProg2/ChallangesGSWP2/21InverseOfNumbers.cpp
+++ 2_StartingWithProg2/ChallangesGSWP2/21InverseOfNumbers.cpp
@@ -23,3 +23,30 @@ int main() {
     cout<<inverse(n)<<endl;
     return 0;
 }
+
+
+// Take the following as input.
+// A number
+// Assume that for a number of n digits, the value of each digit is from 1 to n and is unique. E.g. 32145 is a valid input number.
+
+// Write a function that returns its inverse, where inverse is defined as follows
+
+// Inverse of 32145 is 12543. In 32145, “5” is at 1st place, therefore in 12543, “1” is at 5th place; in 32145, “4” is at 2nd place, therefore in 12543, “2” is at 4th place.
+
+// Print the value returned.
+
+// Input Format
+// Integer
+
+// Constraints
+// 0 < N < 1000000000
+
+// Output Format
+// Integer
+
+// Sample Input
+// 32145
+// Sample Output
+// 12543
+// Explanation
+// Assume that for a number of n digits, the value of each digit is from 1 to n and is unique. E.g. 32145 is a valid input number. Inverse of 32145 is 12543. In 32145, “5” is at 1st place, therefore in 12543, “1” is at 5th place; in 32145, “4” is at 2nd place, therefore in 12543, “2” is at 4th place.
