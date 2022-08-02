928f7faf8dc6687f3bb0548c15e125d7a0e7530b
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:07:40 +0530

    Update 19CheckPrime.cpp

--- 2_StartingWithProg2/ChallangesGSWP2/19CheckPrime.cpp
+++ 2_StartingWithProg2/ChallangesGSWP2/19CheckPrime.cpp
@@ -22,3 +22,18 @@ int main()
     }
     return 0;
 }
+
+
+// Take as input a number N, print "Prime" if it is prime if not Print "Not Prime".
+
+// Input Format
+// Constraints
+// 2 < N <= 1000000000
+
+// Output Format
+// Sample Input
+// 3
+// Sample Output
+// Prime
+// Explanation
+// The output is case specific
