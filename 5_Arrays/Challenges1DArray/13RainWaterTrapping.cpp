f704b1ced4305737792a0d73e4415a4ae0952ae6
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 16:16:53 +0530

    Update 13RainWaterTrapping.cpp

--- 5_Arrays/Challenges1DArray/13RainWaterTrapping.cpp
+++ 5_Arrays/Challenges1DArray/13RainWaterTrapping.cpp
@@ -44,4 +44,28 @@ int main(){
     return 0;
 }
 
+// You are given an input array whose each element represents the height of a line towers. The width of every tower is 1. It starts raining. Water is filled between the gap of towers if possible. You need to find how much water filled between these given towers.
+
+// Example : Imgur
+
+// Input Format
+// The first line consists of number of test cases T. Each test case consists an integer N as number of towers and next line contains the N space separated integers.
+
+// Constraints
+// 1 <= N <= 1000000 1 <= t <= 10 0 <= A[i] <= 10000000
+
+// Output Format
+// Print how much unit of water collected among towers for each test case.
+
+// Sample Input
+// 2
+// 6
+// 3  0  0  2  0  4
+// 12
+// 0  1  0  2  1  0  1  3  2  1  2  1
+// Sample Output
+// 10
+// 6
+
+
 //Rainwater Trapping Problem | Arrays - 5 | DSA-One Course #16 | Anuj Bhaiya
