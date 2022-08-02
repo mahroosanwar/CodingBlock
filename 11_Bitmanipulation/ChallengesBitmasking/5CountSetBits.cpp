3fb3158acdde68a636209a3908c59c2c0cc616a9
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:06:26 +0530

    Update 5CountSetBits.cpp

--- 11_Bitmanipulation/ChallengesBitmasking/5CountSetBits.cpp
+++ 11_Bitmanipulation/ChallengesBitmasking/5CountSetBits.cpp
@@ -31,3 +31,24 @@ int main(){
     
     return 0;
 }
+
+// count number of 1s in binary representation of an integer
+
+// Input Format
+// Input N = Number of Test Cases, followed by N numbers
+
+// Constraints
+// Output Format
+// Number of Set Bits in each number each in a new line
+
+// Sample Input
+// 3
+// 5
+// 4
+// 15
+// Sample Output
+// 2
+// 1
+// 4
+// Explanation
+// Convert Binary to Decimal first and then count number of 1's present in all digits.
