62f4df19ea7e72c05f2e321c0b6fc7d1f769ec33
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:40:14 +0530

    Update 6PrintSeries.cpp

--- 3_StartingWithProg3/ChallangesGSWP3/6PrintSeries.cpp
+++ 3_StartingWithProg3/ChallangesGSWP3/6PrintSeries.cpp
@@ -21,3 +21,31 @@ int main()
     }
     return 0;
 }
+
+// Take the following as input.
+
+// A number (N1)
+// A number (N2)
+// Write a function which prints first N1 terms of the series 3n + 2 which are not multiples of N2.
+
+// Input Format
+// Constraints
+// 0 < N1 < 100 0 < N2 < 100
+
+// Output Format
+// Sample Input
+// 10 
+// 4
+// Sample Output
+// 5 
+// 11 
+// 14 
+// 17 
+// 23 
+// 26 
+// 29 
+// 35 
+// 38 
+// 41
+// Explanation
+// The output will've N1 terms which are not divisible by N2.
