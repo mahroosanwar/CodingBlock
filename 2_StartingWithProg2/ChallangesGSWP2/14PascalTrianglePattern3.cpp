8eeda5fa39e60d3e0e9bc430d6e8c94a0d71ff25
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:01:24 +0530

    Update 14PascalTrianglePattern3.cpp

--- 2_StartingWithProg2/ChallangesGSWP2/14PascalTrianglePattern3.cpp
+++ 2_StartingWithProg2/ChallangesGSWP2/14PascalTrianglePattern3.cpp
@@ -22,3 +22,28 @@ int main()
     }
     return 0;
 }
+
+// Take N (number of rows), print the following pattern (for N = 6)
+// 1
+// 1 1
+// 1 2 1
+// 1 3 3 1
+// 1 4 6 4 1
+// 1 5 10 10 5 1
+
+// Input Format
+// Constraints
+// 0 < N < 100
+
+// Output Format
+// Sample Input
+// 6
+// Sample Output
+// 1  
+// 1	1  
+// 1	2	1  
+// 1	3	3	1  
+// 1	4	6	4	1  
+// 1	5	10	10	5	1
+// Explanation
+// Each number is separated from other by a tab.
