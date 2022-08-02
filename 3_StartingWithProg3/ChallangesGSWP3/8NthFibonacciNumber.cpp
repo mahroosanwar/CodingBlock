9f367e611a9d1e681cfab381b6675dfd660d108f
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:42:09 +0530

    Update 8NthFibonacciNumber.cpp

--- 3_StartingWithProg3/ChallangesGSWP3/8NthFibonacciNumber.cpp
+++ 3_StartingWithProg3/ChallangesGSWP3/8NthFibonacciNumber.cpp
@@ -17,3 +17,18 @@ int main()
     cout << num << endl;
     return 0;
 }
+
+
+// Take N as input. Print Nth Fibonacci Number, given that the first two numbers in the Fibonacci Series are 0 and 1.
+
+// Input Format
+// Constraints
+// 0 <= N <= 1000
+
+// Output Format
+// Sample Input
+// 10
+// Sample Output
+// 55
+// Explanation
+// The 0th fibonnaci is 0 and 1st fibonnaci is 1.
