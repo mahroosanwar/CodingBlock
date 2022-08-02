60b80f7a8945215d14997db523272201aa47ccff
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:55:36 +0530

    Update 8ConversionAnytoAny.cpp

--- 4_Function/ChallengesFunction/8ConversionAnytoAny.cpp
+++ 4_Function/ChallengesFunction/8ConversionAnytoAny.cpp
@@ -32,3 +32,20 @@ int main() {
     cin>>sb>>db>>sn;
     cout<<conversion(sn, sb,db);
 }
+
+
+// Take sb (source number system base), db (destination number system base) and sn (number in source format). Write a function that converts sn to its counterpart in destination number system. Print the value returned.
+
+// Input Format
+// Constraints
+// 0 < N <= 1000000000
+
+// Output Format
+// Sample Input
+// 8
+// 2 
+// 33
+// Sample Output
+// 11011
+// Explanation
+// All input output is as integers and in separate lines.
