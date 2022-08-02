d42f954855b44af3ba80ad7cd77c68be482f34ce
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 16:38:55 +0530

    Update 5ToggleCase.cpp

--- 7_CharacterArray/ChallengesCharactreArray/5ToggleCase.cpp
+++ 7_CharacterArray/ChallengesCharactreArray/5ToggleCase.cpp
@@ -25,3 +25,22 @@ int main(){
     cout << endl;
     return 0;
 }
+
+
+// Take as input S, a string. Write a function that toggles the case of all characters in the string. Print the value returned.
+
+// Input Format
+// String
+
+// Constraints
+// Length of string should be between 1 to 1000.
+
+// Output Format
+// String
+
+// Sample Input
+// abC
+// Sample Output
+// ABc
+// Explanation
+// Toggle Case means to change UpperCase character to LowerCase character and vice-versa.
