f0c6e63f4e9606d808f9b428750c15dd98b0e5b1
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:54:09 +0530

    Update 4KthElementFromLastInLinkedList.cpp

--- 23_LinkedLists/ChallengesLinkedLists/4KthElementFromLastInLinkedList.cpp
+++ 23_LinkedLists/ChallengesLinkedLists/4KthElementFromLastInLinkedList.cpp
@@ -90,3 +90,22 @@ int main(){
 
     return 0;
 }
+
+// Given a linked list with n nodes. Find the kth element from last without computing the length of the linked list.
+
+// Input Format
+// First line contains space separated integers representing the node values of the linked list. The list ends when the input comes as '-1'. The next line contains a single integer k.
+
+// Constraints
+// n < 10^5
+
+// Output Format
+// Output a single line containing the node value at the kth element from last.
+
+// Sample Input
+// 1 2 3 4 5 6 -1
+// 3
+// Sample Output
+// 4
+// Explanation
+// The linked list is 1 2 3 4 5 6. -1 is not included in the list. So the third element from the last is 4
