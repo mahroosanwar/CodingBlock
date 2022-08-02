9ea6e16f3de4c3364286fe9a779148b0ecea9160
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:42:01 +0530

    Update 3Mazepath_D_Count_Print.cpp

--- 16_RecursionVBacktracking/ChallengesRecursion_VBacktracking/3Mazepath_D_Count_Print.cpp
+++ 16_RecursionVBacktracking/ChallengesRecursion_VBacktracking/3Mazepath_D_Count_Print.cpp
@@ -37,3 +37,25 @@ int main(){
     cout << endl << cnt << endl;
     return 0;
 }
+
+// Take as input N1 and N2, both numbers. N1 and N2 is the number of rows and columns on a rectangular board. Our player starts in top-left corner of the board and must reach bottom-right corner. In one move the player can move 1 step horizontally (right) or 1 step vertically (down) or 1 step diagonally (south-east).
+
+// a. Write a recursive function which returns the count of different ways the player can travel across the board. Print the value returned.
+
+// b. Write a recursive function which prints moves for all valid paths across the board (void is the return type for function).
+
+// Input Format
+// Enter the number of rows N1 and number of columns N2
+
+// Constraints
+// None
+
+// Output Format
+// Display the total number of paths and print all the possible paths in a space separated manner
+
+// Sample Input
+// 3
+// 3
+// Sample Output
+// VVHH VHVH VHHV VHD VDH HVVH HVHV HVD HHVV HDV DVH DHV DD
+// 13
