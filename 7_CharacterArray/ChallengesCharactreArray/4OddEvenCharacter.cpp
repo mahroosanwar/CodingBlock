122f3f9fc1026f7102c5f4374267be2676ca4270
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 16:38:15 +0530

    Update 4OddEvenCharacter.cpp

--- 7_CharacterArray/ChallengesCharactreArray/4OddEvenCharacter.cpp
+++ 7_CharacterArray/ChallengesCharactreArray/4OddEvenCharacter.cpp
@@ -26,3 +26,19 @@ int main(){
     cout << endl;
     return 0;
 }
+
+// Take as input S, a string. Write a function that replaces every even character with the character having just higher ASCII code and every odd character with the character having just lower ASCII code. Print the value returned.
+
+// Input Format
+// String
+
+// Constraints
+// Length of string should be between 1 to 1000.
+
+// Output Format
+// String
+
+// Sample Input
+// abcg
+// Sample Output
+// badf
