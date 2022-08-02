127193068b06689b56d87b53c1140f538b9b50fb
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:12:12 +0530

    Update 4LastIndex.cpp

--- 13_DivingIntoRecursion/ChallengesDivingIntoRecursion/4LastIndex.cpp
+++ 13_DivingIntoRecursion/ChallengesDivingIntoRecursion/4LastIndex.cpp
@@ -44,3 +44,26 @@ int main(){
     cout << ans;
     return 0;
 }
+
+
+// Take as input N, the size of array. Take N more inputs and store that in an array. Take as input M, a number. Write a recursive function which returns the last index at which M is found in the array and -1 if M is not found anywhere. Print the value returned.
+
+// Input Format
+// Enter a number N and add N more numbers to an array, then enter number M to be searched
+
+// Constraints
+// None
+
+// Output Format
+// Display the last index at which the number M is found
+
+// Sample Input
+// 5
+// 3
+// 2
+// 1
+// 2
+// 3
+// 2
+// Sample Output
+// 3
