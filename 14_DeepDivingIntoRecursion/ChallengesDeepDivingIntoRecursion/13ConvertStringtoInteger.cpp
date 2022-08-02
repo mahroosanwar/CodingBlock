153cdc74796cfdb4c0d3d7cb47dfe8a8a6bbc511
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:30:49 +0530

    Update 13ConvertStringtoInteger.cpp

--- 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/13ConvertStringtoInteger.cpp
+++ 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/13ConvertStringtoInteger.cpp
@@ -25,3 +25,22 @@ int main(){
 
     return 0;
 }
+
+
+// Take as input str, a number in form of a string. Write a recursive function to convert the number in string form to number in integer form. E.g. for “1234” return 1234. Print the value returned.
+
+// Input Format
+// Enter a number in form of a String
+
+// Constraints
+// 1 <= Length of String <= 10
+
+// Output Format
+// Print the number after converting it into integer
+
+// Sample Input
+// 1234
+// Sample Output
+// 1234
+// Explanation
+// Convert the string to int. Do not use any inbuilt functions.
