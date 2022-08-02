7ae6d47d756e1ecc75e0496db3ecc52063b6b00c
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:15:11 +0530

    Update 8QUICKSORT.cpp

--- 13_DivingIntoRecursion/ChallengesDivingIntoRecursion/8QUICKSORT.cpp
+++ 13_DivingIntoRecursion/ChallengesDivingIntoRecursion/8QUICKSORT.cpp
@@ -46,3 +46,22 @@ int main(){
     }
     return 0;
 }
+
+
+// Given an array A, of N elements. Sort the array using quicksort algorithm.(Note : Use randomized quicksort, otherwise worst case will not pass).
+
+// Input Format
+// A single integer, N, denoting the number of elements in array. Next line contains N integers, denoting the elements of array.
+
+// Constraints
+// 1<=N<=2*10^5
+// |Ai|<=10^9
+
+// Output Format
+// Print in a single line, N spaced integers, denoting the elements of array A in sorted order.
+
+// Sample Input
+// 5
+// 3 6 4 1 2
+// Sample Output
+// 1 2 3 4 6 
