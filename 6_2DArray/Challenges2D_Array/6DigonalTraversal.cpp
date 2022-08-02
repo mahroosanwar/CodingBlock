df893ac77b113b96a1ab01cc77224af221841652
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 16:25:55 +0530

    Update 6DigonalTraversal.cpp

--- 6_2DArray/Challenges2D_Array/6DigonalTraversal.cpp
+++ 6_2DArray/Challenges2D_Array/6DigonalTraversal.cpp
@@ -61,3 +61,21 @@ int main(){
     cout << endl;
     return 0;
 }
+
+
+// Given an array of M x N elements (M rows, N columns), return all elements of the array in diagonal order .
+
+// Input Format
+// Two integers M(row) and N(column) and Second line contains N*M space separated integers denoting the elements of Array.
+
+// Constraints
+// The total number of elements of the given array will not exceed 10,000.
+
+// Output Format
+// display Diagonal
+
+// Sample Input
+// 3 3
+// 1 2 3 4 5 6 7 8 9
+// Sample Output
+// 1 2 4 7 5 3 6 8 9 
