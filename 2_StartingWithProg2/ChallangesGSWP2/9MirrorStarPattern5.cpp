393c783b8ae0d3eefe72eefed7281da8dfec24d7
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 14:47:24 +0530

    Update 9MirrorStarPattern5.cpp

--- 2_StartingWithProg2/ChallangesGSWP2/9MirrorStarPattern5.cpp
+++ 2_StartingWithProg2/ChallangesGSWP2/9MirrorStarPattern5.cpp
@@ -42,3 +42,26 @@ int main()
     }
     return 0;
 }
+
+// Take N (number of rows - only odd numbers allowed), print the following pattern (for N = 5).
+
+//       *
+//    *  *  *  
+// *  *  *  *  *  
+//    *  *  *
+//       *
+// Input Format
+// Constraints
+// 0 < N < 10 (only odd numbers allowed)
+
+// Output Format
+// Sample Input
+// 5
+// Sample Output
+//       *
+//     * * *
+//   * * * * *
+//     * * *
+//       *
+// Explanation
+// Each '*' is separated from other by a tab.
