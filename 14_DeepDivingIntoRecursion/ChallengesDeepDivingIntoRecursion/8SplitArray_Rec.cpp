20e6a729d7964b85a378ed1277535432c666dbe3
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:25:58 +0530

    Update 8SplitArray_Rec.cpp

--- 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/8SplitArray_Rec.cpp
+++ 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/8SplitArray_Rec.cpp
@@ -57,3 +57,36 @@ int main() {
     generateSub(0,v,n,temp,sum,cntways);
     cout<<cntways<<endl;
 }
+
+
+// Take as input N, a number. Take N more inputs and store that in an array.
+
+// a. Write a recursive function which counts the number of ways the array could be split in two groups, so that the sum of items in both groups is equal. Each number in the array must belong to one of the two groups. Print the value returned.
+
+// b. Write a recursive function which keeps track of ways an array could be split in two groups, so that the sum of items in both groups is equal. Each number in the array must belong to one of the two groups. Return type of function should be void. Print the two groups, each time you find a successful split.
+
+// Input Format
+// Take as input N, a number. Take N more inputs and store that in an array.
+
+// Constraints
+// None
+
+// Output Format
+// Display all the groups in a comma separated manner and display the number of ways the array can be split
+
+// Sample Input
+// 6
+// 1
+// 2
+// 3
+// 3
+// 4
+// 5
+// Sample Output
+// 1 2 3 3 and 4 5 
+// 1 3 5 and 2 3 4 
+// 1 3 5 and 2 3 4 
+// 2 3 4 and 1 3 5 
+// 2 3 4 and 1 3 5 
+// 4 5 and 1 2 3 3 
+// 6
