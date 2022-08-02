3322f77929d5969f7211d5f9cc2f6ba1d3b92e8d
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 16:43:29 +0530

    Update 6CharacterType.cpp

--- 7_CharacterArray/ChallengesCharactreArray/6CharacterType.cpp
+++ 7_CharacterArray/ChallengesCharactreArray/6CharacterType.cpp
@@ -23,3 +23,24 @@ int main(){
     cout << endl;
     return 0;
 }
+
+
+// Take the following as input.
+
+// A character (ch) Write a function that returns ‘U’, if it is uppercase; ‘L’ if it is lowercase and ‘I’ otherwise. Print the value returned.
+
+// Input Format
+// Character (ch)
+
+// Constraints
+// No constraints
+
+// Output Format
+// 'L' (if ch is lower-case) 'U' (if ch is upper-case) 'I' (otherwise)
+
+// Sample Input
+// s
+// Sample Output
+// L
+// Explanation
+// Anyone can Do it using Ascii codes. I dare you - "Try to not use ascii codes." ;)
