26b800aaf7f06d46767134a6a239b3840c9bb4a8
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:10:56 +0530

    Update 3FirstIndex.cpp

--- 13_DivingIntoRecursion/ChallengesDivingIntoRecursion/3FirstIndex.cpp
+++ 13_DivingIntoRecursion/ChallengesDivingIntoRecursion/3FirstIndex.cpp
@@ -31,3 +31,27 @@ int main(){
     cout << ans;
     return 0;
 }
+
+
+
+// Take as input N, the size of array. Take N more inputs and store that in an array. Take as input M, a number. Write a recursive function which returns the first index at which M is found in the array and -1 if M is not found anywhere. Print the value returned.
+
+// Input Format
+// Enter a number N and add N more elements to an array, then enter a number M
+
+// Constraints
+// None
+
+// Output Format
+// Display the first index at which number M is found
+
+// Sample Input
+// 5
+// 3
+// 2
+// 1
+// 2
+// 2
+// 2
+// Sample Output
+// 1
