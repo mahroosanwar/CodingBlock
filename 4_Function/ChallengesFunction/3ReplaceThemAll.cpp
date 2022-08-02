aeefeb913c244545d9434ae8672b65e2c41a60c6
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:50:22 +0530

    Update 3ReplaceThemAll.cpp

--- 4_Function/ChallengesFunction/3ReplaceThemAll.cpp
+++ 4_Function/ChallengesFunction/3ReplaceThemAll.cpp
@@ -33,3 +33,22 @@ int main(){
     cout << endl;
     return 0;
 }
+
+
+// Given a integer as a input and replace all the '0' with '5' in the integer
+
+// Input Format
+// Enter an integer n
+
+// Constraints
+// 0<=n<=1000000000000
+
+// Output Format
+// All zeroes are replaced with 5
+
+// Sample Input
+// 102
+// Sample Output
+// 152
+// Explanation
+// Check each digit , if it is nonzero, then no change required but if it is zero then replace it by 5.
