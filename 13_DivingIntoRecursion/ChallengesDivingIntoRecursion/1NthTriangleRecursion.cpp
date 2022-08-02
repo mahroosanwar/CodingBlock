7dbbdcecda69b8e9f969895aec6411f51406a773
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:09:12 +0530

    Update 1NthTriangleRecursion.cpp

--- 13_DivingIntoRecursion/ChallengesDivingIntoRecursion/1NthTriangleRecursion.cpp
+++ 13_DivingIntoRecursion/ChallengesDivingIntoRecursion/1NthTriangleRecursion.cpp
@@ -21,3 +21,20 @@ int main(){
 
     return 0;
 }
+
+
+// Take as input N, a number. Write a recursive function to find Nth triangle where 1st triangle is 1, 2nd triangle is 1 + 2 = 3, 3rd triangle is 1 + 2 + 3 = 6, so on and so forth. Print the value returned.
+
+// Input Format
+// Integer N is the single line of input.
+
+// Constraints
+// 1 <= N <= 100
+
+// Output Format
+// Print the output as a single integer which is the nth triangle.
+
+// Sample Input
+// 3
+// Sample Output
+// 6
