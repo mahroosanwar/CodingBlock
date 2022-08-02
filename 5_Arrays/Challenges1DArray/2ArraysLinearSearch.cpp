b4d56fd02c1b3f9cd52e038a3a89833684b031dc
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:58:43 +0530

    Update 2ArraysLinearSearch.cpp

--- 5_Arrays/Challenges1DArray/2ArraysLinearSearch.cpp
+++ 5_Arrays/Challenges1DArray/2ArraysLinearSearch.cpp
@@ -32,3 +32,27 @@ int main(){
     cout << endl;
     return 0;
 }
+
+// Take as input N, the size of an array. Take N more inputs and store that in an array. Take another number’s input as M. Write a function which returns the index on which M is found in an array, in case M is not found -1 is returned. Print the value returned.
+
+// 1. It reads a number N.
+// 2. Take Another N numbers as an input and store them in an Array.
+// Take another number M as an input.
+// 3. If M is found in the Array the index of M is returned else -1 is returned and print the value returned.
+// Input Format
+// Constraints
+// N cannot be Negative. Range of Numbers can be between -1000000000 to 1000000000. M can be between -1000000000 to 1000000000.
+
+// Output Format
+// Sample Input
+// 5
+// 2
+// 4
+// 6
+// 9
+// 17
+// 17
+// Sample Output
+// 4
+// Explanation
+// Given array = {2, 4, 6, 9, 17}. Target number = 17. Index = 4.
