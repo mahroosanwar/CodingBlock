9248b65a7e73855d00372c3b936591f6f8317e10
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 14:45:27 +0530

    Update 8FibonacciPattern4.cpp

--- 2_StartingWithProg2/ChallangesGSWP2/8FibonacciPattern4.cpp
+++ 2_StartingWithProg2/ChallangesGSWP2/8FibonacciPattern4.cpp
@@ -34,3 +34,24 @@ int main()
     }
     return 0;
 }
+
+// Take N (number of rows), print the following pattern (for N = 4)
+// 0
+// 1 1
+// 2 3 5
+// 8 13 21 34
+
+// Input Format
+// Constraints
+// 0 < N < 100
+
+// Output Format
+// Sample Input
+// 4
+// Sample Output
+// 0 
+// 1    1 
+// 2    3     5 
+// 8   13    21    34
+// Explanation
+// Each number is separated from other by a tab. For given input n, You need to print n(n+1)/2 fibonacci numbers. Kth row contains , next k fibonacci numbers.
