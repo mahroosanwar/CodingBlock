5dd94c91c6a43ece4c0d07d512f60a8e6bc03a60
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:04:12 +0530

    Update 17PatternDoubleSidedArrow.cpp

--- 2_StartingWithProg2/ChallangesGSWP2/17PatternDoubleSidedArrow.cpp
+++ 2_StartingWithProg2/ChallangesGSWP2/17PatternDoubleSidedArrow.cpp
@@ -41,3 +41,34 @@ int main()
     return 0;
 
 }
+
+// Take N as input. For a value of N=7, we wish to draw the following pattern :
+
+//                             1 
+//                         2 1   1 2 
+//                     3 2 1       1 2 3 
+//                 4 3 2 1           1 2 3 4 
+//                     3 2 1       1 2 3 
+//                         2 1   1 2 
+//                             1 
+// Input Format
+// Take N as input.
+
+// Constraints
+// N is odd number.
+
+// Output Format
+// Pattern should be printed with a space between every two values.
+
+// Sample Input
+// 7
+// Sample Output
+//             1 
+//         2 1   1 2 
+//     3 2 1       1 2 3 
+// 4 3 2 1           1 2 3 4 
+//     3 2 1       1 2 3 
+//         2 1   1 2 
+//             1 
+// Explanation
+// Catch the pattern and print it accordingly.
