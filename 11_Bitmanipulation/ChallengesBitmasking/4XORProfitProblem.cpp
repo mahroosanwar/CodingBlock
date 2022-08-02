55792afaa1e0a62f24c3f86b696c1d98a2621a5d
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:05:36 +0530

    Update 4XORProfitProblem.cpp

--- 11_Bitmanipulation/ChallengesBitmasking/4XORProfitProblem.cpp
+++ 11_Bitmanipulation/ChallengesBitmasking/4XORProfitProblem.cpp
@@ -21,3 +21,25 @@ int main(){
 
     return 0;
 }
+
+
+// We are given two coins of value x and y. We have to find the maximum of value of a XOR b where x <= a <= b <= y.
+
+// Input Format
+// We are given two integers x and y
+
+// Constraints
+// l <= r <= 1000
+
+// Output Format
+// Print the maximum value of a XOR b
+
+// Sample Input
+// 5
+// 6
+// Sample Output
+// 3
+// Explanation
+// If a and b are taken to be 5. Then a xor b = 0
+// If a and b are taken to be 6. Then a xor b = 0
+// If a is 5 and b is 6. Then a xor b is 3.
