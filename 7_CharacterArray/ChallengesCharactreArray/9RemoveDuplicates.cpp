a007c92ebe620bbf1ea5adce38409cecaa708353
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 16:45:33 +0530

    Update 9RemoveDuplicates.cpp

--- 7_CharacterArray/ChallengesCharactreArray/9RemoveDuplicates.cpp
+++ 7_CharacterArray/ChallengesCharactreArray/9RemoveDuplicates.cpp
@@ -26,3 +26,23 @@ int main(){
     cout << endl;
     return 0;
 }
+
+// Take as input S, a string. Write a function that removes all consecutive duplicates. Print the value returned.
+
+// Input Format
+// String
+
+// Constraints
+// A string of length between 1 to 1000
+
+// Output Format
+// String
+
+// Sample Input
+// aabccba
+// Sample Output
+// abcba
+// Explanation
+// For the given example, "aabccba", Consecutive Occurrence of a is 2, b is 1, and c is 2.
+
+// After removing all of the consecutive occurences, the Final ans will be : - "abcba".
