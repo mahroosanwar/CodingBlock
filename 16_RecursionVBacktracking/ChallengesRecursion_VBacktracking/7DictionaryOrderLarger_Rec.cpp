384a29d4a34b7e3c15b259ceec4ad1ba0aa57c4d
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:45:09 +0530

    Update 7DictionaryOrderLarger_Rec.cpp

--- 16_RecursionVBacktracking/ChallengesRecursion_VBacktracking/7DictionaryOrderLarger_Rec.cpp
+++ 16_RecursionVBacktracking/ChallengesRecursion_VBacktracking/7DictionaryOrderLarger_Rec.cpp
@@ -32,3 +32,23 @@ int main()
 	}
 	return 0;
 }
+
+// Take as input str, a string. Write a recursive function which prints all the words possible by rearranging the characters of this string which are in dictionary order larger than the given string.
+// The output strings must be lexicographically sorted.
+
+// Input Format
+// Single line input containing a string
+
+// Constraints
+// Length of string <= 10
+
+// Output Format
+// Display all the words which are in dictionary order larger than the string entered in a new line each. The output strings must be sorted.
+
+// Sample Input
+// cab
+
+// Sample Output
+// cba
+// Explanation
+// The possible permutations of string "cab" are "abc" , "acb" ,"bac" , "bca" , "cab" and "cba" . Only one of them is lexicographically greater than "cab". We only print "cba".
