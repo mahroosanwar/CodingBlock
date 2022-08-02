a62da7b2d871d48324fbb5d52bc3ae637bab950b
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:03:14 +0530

    Update 16InvertedPatternHourGlass.cpp

--- 2_StartingWithProg2/ChallangesGSWP2/16InvertedPatternHourGlass.cpp
+++ 2_StartingWithProg2/ChallangesGSWP2/16InvertedPatternHourGlass.cpp
@@ -37,5 +37,39 @@ int main()
     cout << endl;
     }
     return 0;
-    
 }
+
+// Take N as input. For a value of N=5, we wish to draw the following pattern :
+
+//              5                   5 
+//              5 4               4 5 
+//              5 4 3           3 4 5 
+//              5 4 3 2       2 3 4 5 
+//              5 4 3 2 1   1 2 3 4 5 
+//              5 4 3 2 1 0 1 2 3 4 5 
+//              5 4 3 2 1   1 2 3 4 5 
+//              5 4 3 2       2 3 4 5 
+//              5 4 3           3 4 5 
+//              5 4               4 5 
+//              5                   5 
+// Input Format
+// Take N as input.
+
+// Constraints
+// Output Format
+// Pattern should be printed with a space between every two values.
+
+// Sample Input
+// 5
+// Sample Output
+// 5                   5 
+// 5 4               4 5 
+// 5 4 3           3 4 5 
+// 5 4 3 2       2 3 4 5 
+// 5 4 3 2 1   1 2 3 4 5 
+// 5 4 3 2 1 0 1 2 3 4 5 
+// 5 4 3 2 1   1 2 3 4 5 
+// 5 4 3 2       2 3 4 5 
+// 5 4 3           3 4 5 
+// 5 4               4 5 
+// 5                   5 
