96af207f113c63b6d639e646f5d54d4fa1648841
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:36:57 +0530

    Update 2SumofODDEVENplacedDigit.cpp

--- 3_StartingWithProg3/ChallangesGSWP3/2SumofODDEVENplacedDigit.cpp
+++ 3_StartingWithProg3/ChallangesGSWP3/2SumofODDEVENplacedDigit.cpp
@@ -30,3 +30,22 @@ int main()
     cout << sum << endl;
     return 0;
 }
+
+// Take N as input. Print the sum of its odd placed digits and sum of its even placed digits.
+
+// Input Format
+// Constraints
+// 0 < N <= 1000000000
+
+// Output Format
+// Sample Input
+// 2635
+// Sample Output
+// 11
+// 5
+// Explanation
+// 5 is present at 1st position, 3 is present at 2nd position, 6 is present at 3rd position and 2 is present at 4th position.
+
+// Sum of odd placed digits on first line. 5 and 6 are placed at odd position. Hence odd place sum is 5+6=11
+
+// Sum of even placed digits on second line. 3 and 2 are placed at even position. Hence even place sum is 3+2=5
