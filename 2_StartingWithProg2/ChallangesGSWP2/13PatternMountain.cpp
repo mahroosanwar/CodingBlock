b388da8d9e4cc8ba649d409dfda6bda405b2dd05
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 14:51:52 +0530

    Update 13PatternMountain.cpp

--- 2_StartingWithProg2/ChallangesGSWP2/13PatternMountain.cpp
+++ 2_StartingWithProg2/ChallangesGSWP2/13PatternMountain.cpp
@@ -33,3 +33,23 @@ int main()
     }
     return 0;
 }
+
+
+// Take N (number of rows), print the following pattern (for N = 4).
+
+//                        1           1
+//                        1 2       2 1  
+//                        1 2 3   3 2 1
+//                        1 2 3 4 3 2 1   
+// Input Format
+// Constraints
+// 0 < N < 10
+
+// Output Format
+// Sample Input
+// 4
+// Sample Output
+// 1						1
+// 1	2				2	1
+// 1	2	3		3	2	1
+// 1	2	3	4	3	2	1
