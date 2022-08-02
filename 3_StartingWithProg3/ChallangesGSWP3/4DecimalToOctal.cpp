7883cd4d66b6bee8ff56d6a5dfd7122504a1a5a3
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:38:30 +0530

    Update 4DecimalToOctal.cpp

--- 3_StartingWithProg3/ChallangesGSWP3/4DecimalToOctal.cpp
+++ 3_StartingWithProg3/ChallangesGSWP3/4DecimalToOctal.cpp
@@ -18,3 +18,17 @@ int main()
     cout << num << endl;
     return 0;
 }
+
+// Take N (number in decimal format). Write a function that converts it to octal format. Print the value returned.
+
+// Input Format
+// Constraints
+// 0 < N <= 1000000000
+
+// Output Format
+// Sample Input
+// 63
+// Sample Output
+// 77
+// Explanation
+// Both input and output are integers
