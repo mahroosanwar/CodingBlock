e1a7a50163b37891afb41e7b331cf2af3d874fb0
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 14:35:24 +0530

    Update 3LowerUpper.cpp

--- 2_StartingWithProg2/ChallangesGSWP2/3LowerUpper.cpp
+++ 2_StartingWithProg2/ChallangesGSWP2/3LowerUpper.cpp
@@ -31,3 +31,21 @@ int main()
     }
     return 0;
 }
+
+// Print "lowercase" if user enters a character between 'a-z' , Print "UPPERCASE" is character lies between 'A-Z' and print "Invalid" for all other characters like $,.^# etc.
+
+// Input Format
+// Single Character .
+
+// Constraints
+// -
+
+// Output Format
+// lowercase UPPERCASE Invalid
+
+// Sample Input
+// $
+// Sample Output
+// Invalid
+// Explanation
+// -
