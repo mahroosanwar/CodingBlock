b7cea12a3ffeb2b0173718d2120e91cff54c5c37
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:45:45 +0530

    Update 12IncrDecreSequence.cpp

--- 3_StartingWithProg3/ChallangesGSWP3/12IncrDecreSequence.cpp
+++ 3_StartingWithProg3/ChallangesGSWP3/12IncrDecreSequence.cpp
@@ -39,3 +39,29 @@ int main()
     }
     return 0;
 }
+
+
+// Given an array S of size N , check if it is possible to split sequence into two sequences -
+// s1 to si and si to sN such that first sequence is strictly decreasing and second is strictly increasing. Print true/false as output.
+
+// Input Format
+// First line contains a single integer N denoting the size of the input.
+// Next N lines contain a single integer each denoting the elements of the array S.
+
+// Constraints
+// 0 < N < 1000 Each number in sequence S is > 0 and < 1000000000
+
+// Output Format
+// Print boolean output - "true" or "false" defining whether the sequence is increasing - decreasing or not.
+
+// Sample Input
+// 5 
+// 1 
+// 2 
+// 3 
+// 4 
+// 5
+// Sample Output
+// true
+// Explanation
+// Carefully read the conditions to judge which all sequences may be valid. Don't use arrays or lists.
