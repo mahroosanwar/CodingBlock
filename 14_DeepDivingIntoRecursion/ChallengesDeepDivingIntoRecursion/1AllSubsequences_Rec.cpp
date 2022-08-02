142cf8f6604a20b3aa5de319082dd987d5c55f7a
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:17:04 +0530

    Update 1AllSubsequences_Rec.cpp

--- 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/1AllSubsequences_Rec.cpp
+++ 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/1AllSubsequences_Rec.cpp
@@ -48,3 +48,28 @@ int main(){
     }
     return 0;
 }
+
+
+// Print all the subsequences of a string in lexicographical order.
+
+// Input Format
+// First line contains an integer N, the no of strings.
+// Next, N lines follows one string per line.
+
+// Constraints
+// 1 < len(str) < 20
+
+// Output Format
+// No of subsequences one per line
+
+// Sample Input
+// 1  
+// ab
+// Sample Output
+  
+// a  
+// ab  
+// b  
+// Explanation
+// 4 subsequences are printed.
+// Empty string is a subsequence.
