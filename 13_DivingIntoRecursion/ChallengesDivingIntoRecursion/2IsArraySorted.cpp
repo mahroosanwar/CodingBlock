ccc90f7864d68d52d92d78d6ad30b0c3eb9e17d5
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:10:17 +0530

    Update 2IsArraySorted.cpp

--- 13_DivingIntoRecursion/ChallengesDivingIntoRecursion/2IsArraySorted.cpp
+++ 13_DivingIntoRecursion/ChallengesDivingIntoRecursion/2IsArraySorted.cpp
@@ -37,3 +37,25 @@ int main(){
     }
     return 0;
 }
+
+
+
+// Take as input N, the size of array. Take N more inputs and store that in an array. Write a recursive function which returns true if the array is sorted and false otherwise. Print the value returned.
+
+// Input Format
+// Enter a number N and take N more inputs and store in an array
+
+// Constraints
+// None
+
+// Output Format
+// Display the Boolean result
+
+// Sample Input
+// 4
+// 1
+// 2
+// 3
+// 4
+// Sample Output
+// true
