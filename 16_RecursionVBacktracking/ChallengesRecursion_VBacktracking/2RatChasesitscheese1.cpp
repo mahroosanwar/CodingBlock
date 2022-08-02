bdcd3e8f4b6d6ef0071d079ad9a840d56d88de01
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:41:08 +0530

    Update 2RatChasesitscheese1.cpp

--- 16_RecursionVBacktracking/ChallengesRecursion_VBacktracking/2RatChasesitscheese1.cpp
+++ 16_RecursionVBacktracking/ChallengesRecursion_VBacktracking/2RatChasesitscheese1.cpp
@@ -73,3 +73,32 @@ int main()
     }
     return 0;
 }
+
+
+// You are given an N*M grid. Each cell (i,j) in the grid is either blocked, or empty. The rat can move from a position towards left, right, up or down on the grid.
+// Initially rat is on the position (1,1). It wants to reach position (N,M) where it's cheese is waiting for. There exits a unique path in the grid . Find that path and help the rat reach its cheese.
+
+// Input Format
+// First line contains 2 integers N and M denoting the rows and columns in the grid.
+// Next N line contains M characters each. An 'X' in position (i,j) denotes that the cell is blocked and ans 'O' denotes that the cell is empty.
+
+// Constraints
+// 1 <= N , M <= 10
+
+// Output Format
+// Print N lines, containing M integers each. A 1 at a position (i,j) denotes that the (i,j)th cell is covered in the path and a 0 denotes that the cell is not covered in the path.
+// If a path does not exits then print "NO PATH FOUND"
+
+// Sample Input
+// 5 4
+// OXOO
+// OOOX
+// XOXO
+// XOOX
+// XXOO
+// Sample Output
+// 1 0 0 0 
+// 1 1 0 0 
+// 0 1 0 0 
+// 0 1 1 0 
+// 0 0 1 1 
