a520594f18eaf02262d01bca45230a6c3a1a2e20
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 18:01:42 +0530

    Update 9LinkedList_K_Append.cpp

--- 23_LinkedLists/ChallengesLinkedLists/9LinkedList_K_Append.cpp
+++ 23_LinkedLists/ChallengesLinkedLists/9LinkedList_K_Append.cpp
@@ -95,3 +95,29 @@ int main(){
 
     return 0;
 }
+
+// Given a linked list of length N and an integer K , append the last K elements of a linked list to the front. Note that K can be greater than N.
+
+// Input Format
+// First line contains a single integer N denoting the size of the linked list.
+// Second line contains N space separated integers denoting the elements of the linked list.
+// Third line contains a single integer K denoting the number of elements that are to be appended.
+
+// Constraints
+// 1 <= N <= 10^4
+// 1 <= K <= 10^4
+
+// Output Format
+// Display all the elements in the modified linked list.
+
+// Sample Input
+// 7
+// 1 2 2 1 8 5 6
+// 3
+// Sample Output
+// 8 5 6 1 2 2 1
+// Explanation
+// Initially the linked list is
+// 1→ 2→ 3 → 4 → 5 → 6→ null
+// and k = 2. After appending the last two elements to the front , the new linked list looks like
+// 5→ 6→ 1→ 2→ 3 → 4 → null
