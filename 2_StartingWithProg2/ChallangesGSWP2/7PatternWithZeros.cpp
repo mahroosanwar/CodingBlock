c068898fa3a517c0bf8e79cf01abb2a390e52b3f
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 14:44:19 +0530

    Update 7PatternWithZeros.cpp

--- 2_StartingWithProg2/ChallangesGSWP2/7PatternWithZeros.cpp
+++ 2_StartingWithProg2/ChallangesGSWP2/7PatternWithZeros.cpp
@@ -25,3 +25,31 @@ int main()
     }
     return 0;
 }
+
+
+// Take N (number of rows), print the following pattern (for N = 5)
+// 1
+// 2 2
+// 3 0 3
+// 4 0 0 4
+// 5 0 0 0 5
+
+// Input Format
+// There will be an integer in input.
+
+// Constraints
+// 0 < N < 100
+
+// Output Format
+// Print the pattern.
+
+// Sample Input
+// 5
+// Sample Output
+// 1  
+// 2	2  
+// 3	0	3    
+// 4	0	0	4  
+// 5	0	0	0	5
+// Explanation
+// Each number is separated from other by a tab.If row number is n (>1), total character is n. First and last character is n and rest are 0.
