72f214fd0eedf9b51759ffd98f939936691bb46e
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:35:40 +0530

    Update 16ClassAssignment.cpp

--- 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/16ClassAssignment.cpp
+++ 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/16ClassAssignment.cpp
@@ -21,3 +21,31 @@ int main() {
 	}
 	return 0;
 }
+
+// In a mathematics class, Teacher ask Alice to find the number of all n digit distinct integers which is formed by the two distinct digits ai and bi but there is a rule to form n digit integer.
+
+// Rule: She has to form n digit integer by using two digits ai and bi without consecutive bi.
+
+// Alice is very weak in maths section. Help her to find the number of such n digit integers.
+
+// Input Format
+// The first line contains T, the number of test cases. Further T lines contains the value n which is the number of digit in the integer.
+
+// Constraints
+// 1<=T<=20
+// 1<=n<=25
+
+// Output Format
+// For each test case print the number of such n digit integer.
+
+// Sample Input
+// 3
+// 1
+// 2
+// 3
+// Sample Output
+// #1 : 2
+// #2 : 3
+// #3 : 5
+// Explanation
+// For n=1 : integers = a, b . For n=2 : integers = aa, ab, ba For n=3 : integers = aaa, aab, aba, baa, bab
