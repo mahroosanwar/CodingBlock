6479792a6c114c6384c02d0924fe22d7cc94f17f
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:44:45 +0530

    Update 11LCM.cpp

--- 3_StartingWithProg3/ChallangesGSWP3/11LCM.cpp
+++ 3_StartingWithProg3/ChallangesGSWP3/11LCM.cpp
@@ -25,3 +25,22 @@ int main()
     cout << (n1*n2)/hcf << endl;            //LCM = (product of numbers)/HCF
     return 0;
 }
+
+// Take the following as input.
+
+// A number (N1)
+// A number (N2)
+// Write a function which returns the LCM of N1 and N2. Print the value returned.
+
+// Input Format
+// Constraints
+// 0 < N1 < 1000000000 0 < N2 < 1000000000
+
+// Output Format
+// Sample Input
+// 4 
+// 6
+// Sample Output
+// 12
+// Explanation
+// The smallest number that is divisible by both N1 and N2 is called the LCM of N1 and N2.
