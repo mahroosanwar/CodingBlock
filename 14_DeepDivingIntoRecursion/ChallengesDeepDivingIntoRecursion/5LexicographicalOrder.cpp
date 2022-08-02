efd13bf7e22ddd7f7265829dd4e320a709254719
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:22:37 +0530

    Update 5LexicographicalOrder.cpp

--- 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/5LexicographicalOrder.cpp
+++ 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/5LexicographicalOrder.cpp
@@ -28,3 +28,19 @@ int main(){
     }
     return 0;
 }
+
+// Take as input N, a number. Write a recursive function which prints counting from 0 to N in lexicographical order. In lexicographical (dictionary) order 10, 100 and 109 will be printed before 11.
+
+// Input Format
+// Enter a number N.
+
+// Constraints
+// None
+
+// Output Format
+// Display all the numbers upto N in a lexicographical order
+
+// Sample Input
+// 10
+// Sample Output
+// 0 1 10 2 3 4 5 6 7 8 9 
