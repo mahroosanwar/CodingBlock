aba7ade20ccdea545b145f63e830b601c765eb38
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 18:03:38 +0530

    Update 11SumoftwoLinkedLists.cpp

--- 23_LinkedLists/ChallengesLinkedLists/11SumoftwoLinkedLists.cpp
+++ 23_LinkedLists/ChallengesLinkedLists/11SumoftwoLinkedLists.cpp
@@ -115,3 +115,25 @@ int main(){
 
     return 0;
 }
+
+// Given two numbers represented by two linked lists, write a program that prints the sum list. The sum list is linked list representation of addition of two input numbers in linked lists. It is not allowed to modify the lists. Also, not allowed to use explicit extra space (Use Recursion).
+
+// Input Format
+// First line contains N,M, number of nodes in the 1st and 2nd list.
+// Next line contains N nodes of 1st list.
+// Next line contains M nodes of 2nd list.
+
+// Constraints
+// 0<N<10^6
+
+// Output Format
+// Print the sum list after adding the two linked lists.
+
+// Sample Input
+// 3 3
+// 5 6 3
+// 8 4 2
+// Sample Output
+// 1 4 0 5
+// Explanation
+// After adding the two numbers represented by linked lists i.e. 563 + 842 =1405 is represented as sum list. Sum list =1 -> 4 -> 0 -> 5.
