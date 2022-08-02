c4220414f2c408f910b09d8e66cd6e9035ed84dc
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:51:26 +0530

    Update 1MergeSortedLinkedLists.cpp

--- 23_LinkedLists/ChallengesLinkedLists/1MergeSortedLinkedLists.cpp
+++ 23_LinkedLists/ChallengesLinkedLists/1MergeSortedLinkedLists.cpp
@@ -90,3 +90,33 @@ int main(){
     }    
     return 0;
 }
+
+
+// Given 2 sorted linked lists , merge the two given sorted linked list and print the final Linked List.
+
+// Input Format
+// First Line contains T the number of test cases.
+// For each test case :
+// Line 1 : N1 the size of list 1
+// Line 2 : N1 elements for list 1
+// Line 3 : N2 the size of list 2
+// Line 4 : N2 elements for list 2
+
+// Constraints
+// 1 <= T <= 1000
+// 0<= N1, N2 <= 10^6
+// -10^7 <= Ai <= 10^7
+
+// Output Format
+// For each testcase , print the node data of merged linked list.
+
+// Sample Input
+// 1
+// 4
+// 1 3 5 7
+// 3
+// 2 4 6
+// Sample Output
+// 1 2 3 4 5 6 7 
+// Explanation
+// The two lists after merging give the sorted output as [1,2,3,4,5,6,7] .
