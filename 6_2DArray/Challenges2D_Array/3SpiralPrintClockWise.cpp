c8d807a88a920b8b57f83071fea60d1c8fc08de1
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 16:23:09 +0530

    Update 3SpiralPrintClockWise.cpp

--- 6_2DArray/Challenges2D_Array/3SpiralPrintClockWise.cpp
+++ 6_2DArray/Challenges2D_Array/3SpiralPrintClockWise.cpp
@@ -45,3 +45,26 @@ int main(){
     cout << endl;
     return 0;
 }
+
+
+// Take as input a 2-d array.Print the 2-D array in sprial form clockwise.
+
+// Input Format
+// Two integers M(row) and N(colomn) and further M * N integers(2-d array numbers).
+
+// Constraints
+// Both M and N are between 1 to 10.
+
+// Output Format
+// All M * N integers separated by commas with 'END' written in the end(as shown in example).
+
+// Sample Input
+// 4 4
+// 11 12 13 14
+// 21 22 23 24
+// 31 32 33 34
+// 41 42 43 44
+// Sample Output
+// 11, 12, 13, 14, 24, 34, 44, 43, 42, 41, 31, 21, 22, 23, 33, 32, END
+// Explanation
+// For spiral level clockwise traversal, Go for first row-> last column ->last row -> first column and then do the same traversal for the remaining matrix .
