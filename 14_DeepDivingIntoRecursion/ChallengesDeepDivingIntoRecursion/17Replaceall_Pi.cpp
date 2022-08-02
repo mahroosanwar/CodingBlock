1c5ebf4852502543ebc27c61e0d0436ba67bfa80
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:36:25 +0530

    Update 17Replaceall_Pi.cpp

--- 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/17Replaceall_Pi.cpp
+++ 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/17Replaceall_Pi.cpp
@@ -44,3 +44,27 @@ int main(){
         Replaceallpi(a, 0);
     }
 }
+
+// Replace all occurrences of pi with 3.14
+
+// Input Format
+// Integer N, no of lines with one string per line
+
+// Constraints
+// 0 < N < 1000
+// 0 <= len(str) < 1000
+
+// Output Format
+// Output result one per line
+
+// Sample Input
+// 3
+// xpix
+// xabpixx3.15x
+// xpipippixx
+// Sample Output
+// x3.14x
+// xab3.14xx3.15x
+// x3.143.14p3.14xx
+// Explanation
+// All occurrences of pi have been replaced with "3.14".
