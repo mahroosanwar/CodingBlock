6f523fa4abd295b078a8f594c0bcdfdcaf519143
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 14:49:34 +0530

    Update 11PatternTriangle.cpp

--- 2_StartingWithProg2/ChallangesGSWP2/11PatternTriangle.cpp
+++ 2_StartingWithProg2/ChallangesGSWP2/11PatternTriangle.cpp
@@ -21,3 +21,26 @@ int main()
     cout << endl;
     }
 }
+
+
+
+// Take N (number of rows), print the following pattern (for N = 4).
+
+//                        1 
+//                      2 3 2
+//                    3 4 5 4 3
+//                  4 5 6 7 6 5 4
+// Input Format
+// Constraints
+// 0 < N < 10
+
+// Output Format
+// Sample Input
+// 4
+// Sample Output
+//                         1
+// 		2	3	2
+// 	3	4	5	4	3
+// 4	5	6	7	6	5	4
+// Explanation
+// Each number is separated from other by a tab.
