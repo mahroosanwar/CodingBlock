b241def5ae3f3675e603a6e08595fd925846bdb2
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:03:59 +0530

    Update 2UniqueNumber_I.cpp

--- 11_Bitmanipulation/ChallengesBitmasking/2UniqueNumber_I.cpp
+++ 11_Bitmanipulation/ChallengesBitmasking/2UniqueNumber_I.cpp
@@ -22,3 +22,23 @@ int main(){
 
     return 0;
 }
+
+
+// We are given an array containg n numbers. All the numbers are present twice except for one number which is only present once. Find the unique number without taking any extra spaces and in linear time. ( Hint - Use Bitwise )
+
+// Input Format
+// First line contains the number n. Second line contains n space separated number.
+
+// Constraints
+// n < 10^5
+
+// Output Format
+// Output a single line containing the unique number
+
+// Sample Input
+// 7
+// 1 1 2 2 3 3 4 
+// Sample Output
+// 4
+// Explanation
+// 4 is present only once
