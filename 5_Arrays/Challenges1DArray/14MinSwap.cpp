0c83c27b7bc96277133d316c00ab9aa9fa004a25
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 16:19:16 +0530

    Update 14MinSwap.cpp

--- 5_Arrays/Challenges1DArray/14MinSwap.cpp
+++ 5_Arrays/Challenges1DArray/14MinSwap.cpp
@@ -70,4 +70,27 @@ int main(){
 	
 	// Output will be 5
 	cout << minSwaps(a, n);
+	
+	return 0;
 }
+
+
+// Given an array of n distinct elements. Find the minimum number of swaps required to sort the array in strictly increasing order.
+
+// Input Format
+// N
+// N elements
+
+// Constraints
+// N<=10^7
+
+// Output Format
+// Minimum steps
+
+// Sample Input
+// 4
+// 2 8 5 4
+// Sample Output
+// 1
+// Explanation
+// swap 8 with 4.
