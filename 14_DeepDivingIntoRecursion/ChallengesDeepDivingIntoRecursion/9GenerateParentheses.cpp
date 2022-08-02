170af8ce00ba46c36b353fe6ec88dfd35ac238e7
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:26:56 +0530

    Update 9GenerateParentheses.cpp

--- 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/9GenerateParentheses.cpp
+++ 14_DeepDivingIntoRecursion/ChallengesDeepDivingIntoRecursion/9GenerateParentheses.cpp
@@ -28,3 +28,22 @@ int main(){
     GenerateParentheses(a, n, 0, 0, 0);
     return 0;
 }
+
+// Given an integer 'n'. Print all the possible pairs of 'n' balanced parentheses.
+// The output strings should be printed in the sorted order considering '(' has higher value than ')'.
+
+// Input Format
+// Single line containing an integral value 'n'.
+
+// Constraints
+// 1<=n<=11
+
+// Output Format
+// Print the balanced parentheses strings with every possible solution on new line.
+
+// Sample Input
+// 2
+// Sample Output
+// ()() 
+// (()) 
+// Code Editor
