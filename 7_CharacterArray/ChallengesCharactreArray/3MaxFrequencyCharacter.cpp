7ce68bccbc743d534f049dc61111298faa570578
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 16:37:28 +0530

    Update 3MaxFrequencyCharacter.cpp

--- 7_CharacterArray/ChallengesCharactreArray/3MaxFrequencyCharacter.cpp
+++ 7_CharacterArray/ChallengesCharactreArray/3MaxFrequencyCharacter.cpp
@@ -33,3 +33,22 @@ int main(){
     cout << endl;
     return 0;
 }
+
+
+// Take as input S, a string. Write a function that returns the character with maximum frequency. Print the value returned.
+
+// Input Format
+// String
+
+// Constraints
+// A string of length between 1 to 1000.
+
+// Output Format
+// Character
+
+// Sample Input
+// aaabacb
+// Sample Output
+// a
+// Explanation
+// For the given input string, a appear 4 times. Hence, it is the most frequent character.
