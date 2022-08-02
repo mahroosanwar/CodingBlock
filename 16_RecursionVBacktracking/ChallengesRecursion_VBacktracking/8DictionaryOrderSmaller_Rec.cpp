3eb1659164ec354c7d20d74d61981779f0909481
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:49:23 +0530

    Update 8DictionaryOrderSmaller_Rec.cpp

--- 16_RecursionVBacktracking/ChallengesRecursion_VBacktracking/8DictionaryOrderSmaller_Rec.cpp
+++ 16_RecursionVBacktracking/ChallengesRecursion_VBacktracking/8DictionaryOrderSmaller_Rec.cpp
@@ -51,3 +51,24 @@ int main()
 	
 }
 
+
+// Take as input str, a string. Write a recursive function which returns all the words possible by rearranging the characters of this string which are in dictionary order smaller than the given string. The output strings must be lexicographically sorted.
+
+// Input Format
+// Single line input containing a string
+
+// Constraints
+// Length of string <= 25
+
+// Output Format
+// Display all the words which are in dictionary order smaller than the string entered in a new line each. The output strings must be sorted.
+
+// Sample Input
+// cab
+// Sample Output
+// abc
+// acb
+// bac
+// bca
+// Explanation
+// The possible permutations of string "cab" are "abc" , "acb" ,"bac" , "bca" , "cab" and "cba" . Only four of them are lexicographically less than "cab". We print them in lexicographical order.
