f3a95e058bcf6d5797b6e96b578c289dfdfe7627
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:41:10 +0530

    Update 7PrintReverse.cpp

--- 3_StartingWithProg3/ChallangesGSWP3/7PrintReverse.cpp
+++ 3_StartingWithProg3/ChallangesGSWP3/7PrintReverse.cpp
@@ -25,3 +25,17 @@ int main()
     cout << num << endl;
     return 0;
 }
+
+// Take N as input, Calculate it's reverse also Print the reverse.
+
+// Input Format
+// Constraints
+// 0 <= N <= 1000000000
+
+// Output Format
+// Sample Input
+// 123456789
+// Sample Output
+// 987654321
+// Explanation
+// You've to calculate the reverse in a number, not just print the reverse.
