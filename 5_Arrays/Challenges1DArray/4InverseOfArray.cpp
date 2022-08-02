6dcf659fd35c0a71dad7815fdab020646a692e1a
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 16:00:35 +0530

    Update 4InverseOfArray.cpp

--- 5_Arrays/Challenges1DArray/4InverseOfArray.cpp
+++ 5_Arrays/Challenges1DArray/4InverseOfArray.cpp
@@ -32,3 +32,25 @@ int main(){
     cout << endl;
     return 0;
 }
+
+
+// Take as input N, a number. Take N more inputs and store that in an array. Write a recursive function which inverses the array. Print the values of inverted array
+
+// Input Format
+// Enter a number N and take N more inputs
+
+// Constraints
+// None
+
+// Output Format
+// Display the values of the inverted array in a space separated manner
+
+// Sample Input
+// 5
+// 0 2 4 1 3
+// Sample Output
+// 0 3 1 4 2
+// Explanation
+// Swap element with index
+
+// for eg : element 4 at index 2 becomes element 2 at index 4
