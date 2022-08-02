7fed6390ffda2c3774e8cd661388b454624fd901
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:53:44 +0530

    Update 6PrintArmstrongNum.cpp

--- 4_Function/ChallengesFunction/6PrintArmstrongNum.cpp
+++ 4_Function/ChallengesFunction/6PrintArmstrongNum.cpp
@@ -46,3 +46,24 @@ int main(){
     return 0;
 }
  
+
+// Take the following as input.
+
+// A number (N1)
+// A number (N2)
+// Write a function which prints all armstrong numbers between N1 and N2 (inclusive).
+
+// 371 is an Armstrong number as 371 = 3^3 + 7^3 + 1^3
+
+// Input Format
+// Constraints
+// 0 < N1 < 100 N1 < N2 < 10000
+
+// Output Format
+// Sample Input
+// 400
+// 1000
+// Sample Output
+// 407
+// Explanation
+// Each number in output is in separate line.
