5ece0bfdd3aa53e455dbdfedd1a43edaa91a4b54
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 16:35:40 +0530

    Update 1isPalindrome.cpp

--- 7_CharacterArray/ChallengesCharactreArray/1isPalindrome.cpp
+++ 7_CharacterArray/ChallengesCharactreArray/1isPalindrome.cpp
@@ -29,3 +29,22 @@ int main()
     cout << endl;
     return 0;
 }
+
+
+// Take as input S, a string. Write a function that returns true if the string is a palindrome and false otherwise. Print the value returned.
+
+// Input Format
+// String
+
+// Constraints
+// String length between 1 to 1000 characters
+
+// Output Format
+// Boolean
+
+// Sample Input
+// abba
+// Sample Output
+// true
+// Explanation
+// A string is said to be palindrome if reverse of the string is same as string. For example, “abba” is palindrome as it's reverse is "abba", but “abbc” is not palindrome as it's reverse is "cbba".
