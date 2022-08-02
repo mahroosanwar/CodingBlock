19aa06f4ce1e0ea85814fce14b17e751faefaa57
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 16:36:27 +0530

    Update 2CountPalindromicSubstring.cpp

--- 7_CharacterArray/ChallengesCharactreArray/2CountPalindromicSubstring.cpp
+++ 7_CharacterArray/ChallengesCharactreArray/2CountPalindromicSubstring.cpp
@@ -44,3 +44,22 @@ int main(){
     cout << endl;
     return 0;
 }
+
+
+// Take as input S, a string. Write a program that gives the count of substrings of this string which are palindromes and Print the ans.
+
+// Input Format
+// Single line input containing a string
+
+// Constraints
+// Length of string is between 1 to 1000.
+
+// Output Format
+// Integer output showing the number of palindromic substrings.
+
+// Sample Input
+// abc
+// Sample Output
+// 3
+// Explanation
+// For the given sample case , the palindromic substrings of the string abc are "a","b" and "c".So, the ans is 3.
