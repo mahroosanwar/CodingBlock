e9e6c8962d3237d3795170adb5843608af045726
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 16:46:11 +0530

    Update 10StringCompression.cpp

--- 7_CharacterArray/ChallengesCharactreArray/10StringCompression.cpp
+++ 7_CharacterArray/ChallengesCharactreArray/10StringCompression.cpp
@@ -24,3 +24,22 @@ int main(){
     cout << endl;
     return 0;
 }
+
+
+// Take as input S, a string. Write a function that does basic string compression. Print the value returned. E.g. for input “aaabbccds” print out a3b2c2d1s1.
+
+// Input Format
+// A single String S
+
+// Constraints
+// 1 < = length of String < = 1000
+
+// Output Format
+// The compressed String.
+
+// Sample Input
+// aaabbccds
+// Sample Output
+// a3b2c2d1s1
+// Explanation
+// In the given sample test case 'a' is repeated 3 times consecutively, 'b' is repeated twice, 'c' is repeated twice and 'd and 's' occurred only once.
