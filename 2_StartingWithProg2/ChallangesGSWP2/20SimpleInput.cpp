75b9d39c9fee7a6a63540c9e1eb898c0688ec1b6
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 15:08:43 +0530

    Update 20SimpleInput.cpp

--- 2_StartingWithProg2/ChallangesGSWP2/20SimpleInput.cpp
+++ 2_StartingWithProg2/ChallangesGSWP2/20SimpleInput.cpp
@@ -20,3 +20,26 @@ int main()
     }
     return 0;
 }
+
+
+// Given a list of numbers, stop processing input after the cumulative sum of all the input becomes negative.
+
+// Input Format
+// A list of integers to be processed
+
+// Constraints
+// All numbers input are integers between -1000 and 1000.
+
+// Output Format
+// Print all the numbers before the cumulative sum become negative.
+
+// Sample Input
+// 1
+// 2
+// 88
+// -100
+// 49
+// Sample Output
+// 1
+// 2
+// 88
