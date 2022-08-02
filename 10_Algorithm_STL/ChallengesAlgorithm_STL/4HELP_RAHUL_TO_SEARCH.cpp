cda6a88930bb42116a8118f3f34edb0a6b936aa5
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-02 17:00:01 +0530

    Update 4HELP_RAHUL_TO_SEARCH.cpp

--- 10_Algorithm_STL/ChallengesAlgorithm_STL/4HELP_RAHUL_TO_SEARCH.cpp
+++ 10_Algorithm_STL/ChallengesAlgorithm_STL/4HELP_RAHUL_TO_SEARCH.cpp
@@ -44,3 +44,30 @@ int main() {
     cout<<rahul__key(A,n,key);
     return 0;
 }
+
+
+// Rahul had a sorted array of numbers from which he had to find a given number quickly. His friend by mistake rotated the array. Now Rahul doesn't have time to sort the elements again. Help him to quickly find the given number from the rotated array.
+
+// Hint - Think Binary Search!
+
+
+
+// Input Format
+// The first line contains N - the size of the array. the next N lines contain the numbers of the array.The next line contains the item to be searched.
+
+// Constraints
+// Output Format
+// Output the index of number in the array to be searched. Output -1 if the number is not found.
+
+// Sample Input
+// 5
+// 4
+// 5
+// 1
+// 2
+// 3
+// 2
+// Sample Output
+// 3
+// Explanation
+// The given rotated array is [4, 5, 1, 2, 3]. The element to be searched is 2 whose index is 3.
