7e1dbb4b9db01441dcb09257ecf44642e15ef13b
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:13:37 +0530

    Update 3Structurally_Identical_Binary_Tree.cpp

--- 28_Binary Search Tree/Challenges Trees/3Structurally_Identical_Binary_Tree.cpp
+++ 28_Binary Search Tree/Challenges Trees/3Structurally_Identical_Binary_Tree.cpp
@@ -67,3 +67,23 @@ int main() {
 
     return 0;
 }
+
+
+// Given two trees check if they are structurally identically. Structurally identical trees are trees that have same structure. They may or may not have the same data though.
+
+// Input Format
+// Enter the values of all the nodes in the binary tree in pre-order format where true suggest the node exists and false suggests it is NULL
+
+// Constraints
+// 1 <= N <= 10^4
+
+// Output Format
+// Display the Boolean result
+
+// Sample Input
+// 10 true 20 true 40 false false true 50 false false true 30 true 60 false false true 73 false false
+// 10 true 20 true 40 false false true 50 false false true 30 true 60 false false true 73 false false
+// Sample Output
+// true
+// Explanation
+// The given two trees have the exact same structure and hence we print true.
