35efc48a5d4dd0b2ba8a665679f83c5ebff25d6b
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:20:57 +0530

    Update 10Create_tree_Using_preorder_and_inorder.cpp

--- 28_Binary Search Tree/Challenges Trees/10Create_tree_Using_preorder_and_inorder.cpp
+++ 28_Binary Search Tree/Challenges Trees/10Create_tree_Using_preorder_and_inorder.cpp
@@ -96,3 +96,33 @@ int main() {
     printTree(root);
     return 0;
 }
+
+
+// Given preorder and inorder create the tree
+
+// Input Format
+// Enter the size of the preorder array N then add N more elements and store in the array denoting the preorder traversal of the tree. Then enter the size of the inorder array M and add M more elements and store the inorder traversal of the array.
+
+// Constraints
+// 1 <= N, M <= 10^4
+
+// Output Format
+// Display the tree using a modified preorder function. For each node , first print its left child's data , then the data of the root itself , then the data of its right child. Do this for each node in a new line in preorder manner. If one of the children does not exist print END in its place. Refer to the sample testcase.
+
+// Sample Input
+// 3
+// 1 2 3
+// 3
+// 3 2 1
+// Sample Output
+// 2 => 1 <= END
+// 3 => 2 <= END
+// END => 3 <= END
+// Explanation
+// The above tree looks like
+
+//          1
+//        /
+//      2
+//    /
+//  3
