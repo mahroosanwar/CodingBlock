bbb11cb6ac8bef5d7090bd545a693698afae3e36
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:17:21 +0530

    Update 7_Remove_the_leaves_Binary_Tree.cpp

--- 28_Binary Search Tree/Challenges Trees/7_Remove_the_leaves_Binary_Tree.cpp
+++ 28_Binary Search Tree/Challenges Trees/7_Remove_the_leaves_Binary_Tree.cpp
@@ -89,3 +89,23 @@ int main() {
 
     return 0;
 }
+
+
+Given a binary tree, remove all the leaves from the tree
+
+Input Format
+Enter the value of the nodes of the tree
+
+Constraints
+None
+
+Output Format
+Display the tree in which all the leaves have been removed in pre-order traversal in the following format :
+Left->data => Root->data <= Right->Data
+Output END if left or right node is NULL
+
+Sample Input
+50 true 12 true 18 false false false false
+Sample Output
+12 => 50 <= END
+END => 12 <= END
