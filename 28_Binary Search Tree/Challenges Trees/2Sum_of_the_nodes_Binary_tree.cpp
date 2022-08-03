4fc9f88b5415e5c2c7e0161b7e969a7955cf410c
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:13:00 +0530

    Update 2Sum_of_the_nodes_Binary_tree.cpp

--- 28_Binary Search Tree/Challenges Trees/2Sum_of_the_nodes_Binary_tree.cpp
+++ 28_Binary Search Tree/Challenges Trees/2Sum_of_the_nodes_Binary_tree.cpp
@@ -67,3 +67,35 @@ int main() {
 
     return 0;
 }
+
+// Given a binary tree find sum of all the nodes.
+
+// Input Format
+// Enter the value of the node then the Boolean choice i.e whether the node has a left child , then enter the left child's data . The input acts in a recursive manner i.e when the left child's children are made only then we move onto the parent's right child
+
+// Constraints
+// None
+
+// Output Format
+// Display the sum of all the nodes
+
+// Sample Input
+// 50 true 25 true 12 false false false true 75 true 62 false false false
+// Sample Output
+// 224
+// Explanation
+// If we enter the following values
+
+// 50 true 25 true 12 false false false true 75 true 62 false false false
+
+// the tree would look like :
+
+// 25 => 50 <= 75
+
+// 12 => 25 <= END
+
+// END => 12 <= END
+
+// 62 => 75 <= END
+
+// END => 62 <= END
