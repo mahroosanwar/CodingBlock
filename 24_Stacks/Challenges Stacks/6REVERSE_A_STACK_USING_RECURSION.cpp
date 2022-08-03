57b33307ab639c92d4fec60a5b807a9c6b6c5266
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:01:37 +0530

    Update 6REVERSE_A_STACK_USING_RECURSION.cpp

--- 24_Stacks/Challenges Stacks/6REVERSE_A_STACK_USING_RECURSION.cpp
+++ 24_Stacks/Challenges Stacks/6REVERSE_A_STACK_USING_RECURSION.cpp
@@ -54,3 +54,30 @@ int main(){
 
     return 0;
 }
+
+
+
+// Reverse a Stack using Recursion. Do not use any extra stack.
+
+// Input Format
+// First line contains an integer N (size of the stack).
+// Next N lines follow each containing an integer to be stored in the stack where the last integer is at the top of the stack.
+
+// Constraints
+// 1 <= N <= 10^4
+
+// Output Format
+// Output the values of the reversed stack with each value in one line each.
+
+// Sample Input
+// 3
+// 3
+// 2
+// 1
+// Sample Output
+// 3
+// 2
+// 1
+// Explanation
+// Original Stack = [ 3 , 2 , 1 ] <-TOP
+// Reverse Stack = [ 1 , 2 , 3 ] <-TOP
