a033fb54a86feadf19dce323bc18d1e8b8025071
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:30:13 +0530

    Update 21DeletenodesfromBST.cpp

--- 28_Binary Search Tree/Challenges Trees/21DeletenodesfromBST.cpp
+++ 28_Binary Search Tree/Challenges Trees/21DeletenodesfromBST.cpp
@@ -131,3 +131,44 @@ int main() {
     }
     return 0;
 }
+
+
+
+// You are given an array A1 of integers. You have to form first Binary Search tree and then you are provided another array A2. You have to delete each node from the BST which is present in the array A2. Now you have to print preorder traversal of the tree. It is assured that the elements of array A2 will be present in the tree.
+// Note : In case of node with 2 children, consider its inorder successor as its replacement.
+
+// Input Format
+// First line contains integer t as number of test cases.
+// Each test case contains four lines. First line contains integer n which represents the length of the array A1 and next line contains n space separated integers denoting the elements of array A1. Similarly third line contains integer m which represents the length of the array A2 and next line contains m space separated integers denoting the elements of array A2.
+
+// Constraints
+// 1 < t < 100
+// 1< n,m < 1000
+
+// Output Format
+// Print the preorder traversal of the final resultant tree.
+
+// Sample Input
+// 1
+// 7
+// 5 3 2 4 7 6 8
+// 3
+// 2 3 5
+// Sample Output
+// 6 4 7 8 
+// Explanation
+// Initially the tree looks like
+
+//              5
+//           /      \
+//        3           7
+//     /     \      /    \
+//    2       4    6      8
+// After the deletion , the tree looks like
+
+//                6
+//             /     \
+//            4       7
+//                      \
+//                       8
+// We print the preorder of the final tree.
