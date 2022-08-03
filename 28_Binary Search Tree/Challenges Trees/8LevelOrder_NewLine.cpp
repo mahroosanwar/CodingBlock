0e925aca5879fd72df4b003922cc61c1875828be
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:19:11 +0530

    Update 8LevelOrder_NewLine.cpp

--- 28_Binary Search Tree/Challenges Trees/8LevelOrder_NewLine.cpp
+++ 28_Binary Search Tree/Challenges Trees/8LevelOrder_NewLine.cpp
@@ -75,3 +75,22 @@ int main() {
 
     return 0;
 }
+
+
+// Given a binary tree. Print the level order traversal, make sure each level start at a new line.
+
+// Input Format
+// Enter the values of all the nodes in the binary tree in pre-order format where true suggest the node exists and false suggests it is NULL
+
+// Constraints
+// None
+
+// Output Format
+// Print the level order in which the different levels are in different lines, all the values should be in a space separated manner
+
+// Sample Input
+// 10 true 20 true 40 false false true 50 false false true 30 true 60 false false true 73 false false
+// Sample Output
+// 10 
+// 20 30 
+// 40 50 60 73
