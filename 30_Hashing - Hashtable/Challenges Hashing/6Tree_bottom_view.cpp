6e980a82195d3126bfee5def657f916ee9851f1b
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 18:29:29 +0530

    Update 6Tree_bottom_view.cpp

--- 30_Hashing - Hashtable/Challenges Hashing/6Tree_bottom_view.cpp
+++ 30_Hashing - Hashtable/Challenges Hashing/6Tree_bottom_view.cpp
@@ -95,3 +95,29 @@ int main() {
 
     return 0;
 }
+
+
+// Given a binary tree , print the nodes in left to right manner as visible from below the tree
+
+// Input Format
+// Level order input for the binary tree will be given.
+
+// Constraints
+// No of nodes in the tree can be less than or equal to 10^7
+
+// Output Format
+// A single line containing space separated integers representing the bottom view of the tree
+
+// Sample Input
+// 1 2 3 4 5 6 -1 -1 -1 -1 -1 -1 -1
+// Sample Output
+// 4 2 6 3
+// Explanation
+// The tree looks like
+
+//              1
+//           /      \
+//        2           3
+//     /     \       /
+//    4       5     6
+// (Note that 5 and 6 are at the same position so we consider the right one to lower)
