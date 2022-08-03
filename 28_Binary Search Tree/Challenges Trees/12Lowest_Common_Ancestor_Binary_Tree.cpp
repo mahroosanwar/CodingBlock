399cd8330645a10aabd93a27f4e2f6898d09d97c
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:22:48 +0530

    Update 12Lowest_Common_Ancestor_Binary_Tree.cpp

--- 28_Binary Search Tree/Challenges Trees/12Lowest_Common_Ancestor_Binary_Tree.cpp
+++ 28_Binary Search Tree/Challenges Trees/12Lowest_Common_Ancestor_Binary_Tree.cpp
@@ -75,3 +75,23 @@ int main()  {
 
     return 0;
 }
+
+
+
+// Find LCA(Lowest Common Ancestor) of two elements in a Binary Tree.
+
+// Input Format
+// Enter the values of all the nodes in the binary tree in pre-order format where true suggest the node exists and false suggests it is NULL next two line contains 2 element of BT
+
+// Constraints
+// None
+
+// Output Format
+// Display the LCA value.
+
+// Sample Input
+// 10 true 20 true 40 false false true 50 false false true 30 true 60 false false true 73 false false
+// 50
+// 60
+// Sample Output
+// 10
