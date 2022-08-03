9af685cec6e283419ed14d02fbe8a30644073151
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:08:49 +0530

    Update 3First_negative_integer_in_every_window_of_size_k.cpp

--- 25Queues/Challenges Queue/3First_negative_integer_in_every_window_of_size_k.cpp
+++ 25Queues/Challenges Queue/3First_negative_integer_in_every_window_of_size_k.cpp
@@ -53,3 +53,27 @@ int main() {
 
     return 0;
 }
+
+
+// You are given given an array and a positive integer k, find the first negative integer for each and every window(contiguous subarray) of size k. If a window does not contain a negative integer, then print 0 for that window.
+
+// Input Format
+// First line contains integer t as number of test cases. Each test case contains two lines. First line contains two integers n and k where n is length of the array and k is the size of window and second line contains n space separated integer.
+
+// Constraints
+// 1 < t < 10 1< n, k < 10000000
+
+// Output Format
+// For each test case you have to print the required output as given below.
+
+// Sample Input
+// 2
+// 8 3
+// 12 -1 -7 8 -15 30 16 28
+//  8 4
+// 12 -1 -7 8 -15 30 16 28
+// Sample Output
+// -1 -1 -7 -15 -15 0 
+// -1 -1 -7 -15 -15
+// Explanation
+// For first test case : Subarray of window size 3 is ( 12 -1 -7), (-1, -7, 8), and so on.. Take first negative number from each window and print them.
