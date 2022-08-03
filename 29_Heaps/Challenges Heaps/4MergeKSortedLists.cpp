abbbc60d019cbdfebcae8c2fa62e1e4729ded74d
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 18:24:28 +0530

    Update 4MergeKSortedLists.cpp

--- 29_Heaps/Challenges Heaps/4MergeKSortedLists.cpp
+++ 29_Heaps/Challenges Heaps/4MergeKSortedLists.cpp
@@ -115,3 +115,21 @@ int main() {
     printList(ans);
     return 0;
 }
+
+// Given K sorted linked lists of equal sizes. The task is to merge them in such a way that after merging they will be a single sorted linked list.
+
+// Input Format
+// First line contains 2 space separated integers K and N.
+// Next lines contain N*K space separated integers
+
+// Constraints
+// None
+
+// Output Format
+// Single line consisting of space separated numbers
+
+// Sample Input
+// 3 4
+// 5 5 5 9 3 13 14 17 1 8 11 18 
+// Sample Output
+// 1 3 5 5 5 8 9 11 13 14 17 18 
