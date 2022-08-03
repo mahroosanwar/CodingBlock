4cf47a44d93f4e005f4520efbd73afd132077a05
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:04:43 +0530

    Update 10Dequeue_efficient_queue_using_stack.cpp

--- 24_Stacks/Challenges Stacks/10Dequeue_efficient_queue_using_stack.cpp
+++ 24_Stacks/Challenges Stacks/10Dequeue_efficient_queue_using_stack.cpp
@@ -25,3 +25,18 @@ int main() {
 	}
 	return 0;
 }
+
+
+// Implement a Queue using two stacks Make it Dequeue efficient.
+
+// Input Format
+// Enter the size of the queue N add 0 - N-1 numbers in the queue
+
+// Constraints
+// Output Format
+// Display the numbers in the order they are dequeued and in a space separated manner
+
+// Sample Input
+// 5
+// Sample Output
+// 0 1 2 3 4
