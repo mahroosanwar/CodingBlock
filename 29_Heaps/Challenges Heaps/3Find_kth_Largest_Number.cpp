7d6e7ef8d5670f0ef6001fc4e301f7c498f19da1
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 18:23:49 +0530

    Update 3Find_kth_Largest_Number.cpp

--- 29_Heaps/Challenges Heaps/3Find_kth_Largest_Number.cpp
+++ 29_Heaps/Challenges Heaps/3Find_kth_Largest_Number.cpp
@@ -40,3 +40,23 @@ int main() {
 
     return 0;
 }
+
+
+// Find the kth largest element in an unsorted array.
+// Note:It is the kth largest element in the sorted order, not the kth distinct element.
+
+// Input Format
+// First line contains integer n as size of array. Second line contains the value of k. Third line contains n integers as element of array.
+
+// Constraints
+// 1 ≤ k ≤ array's length.
+
+// Output Format
+// Print the kth largest element as output.
+
+// Sample Input
+// 6
+// 2
+// 3 2 1 5 6 4
+// Sample Output
+// 5
