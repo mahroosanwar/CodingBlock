7ce66400d0f672dcbbf1d307abc8938940a982de
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 18:25:06 +0530

    Update 5Top_k_most_frequent_number_in_a_stream.cpp

--- 29_Heaps/Challenges Heaps/5Top_k_most_frequent_number_in_a_stream.cpp
+++ 29_Heaps/Challenges Heaps/5Top_k_most_frequent_number_in_a_stream.cpp
@@ -48,3 +48,22 @@ int main(int argc, char const *argv[])
     }
     return 0;
 }
+
+
+// Given an array of n numbers. Your task is to read numbers from the array and keep at-most K numbers at the top (according to their decreasing frequency) every time a new number is read. We basically need to print top k numbers sorted by frequency when input stream has included k distinct elements, else need to print all distinct elements sorted by frequency. If frequency of two numbers are same then print them in increasing order.
+
+// Input Format
+// First line contains integer t as number of test cases. Each test case contains following input. First line contains integer n and k, n represents the length of the array and next line contains n space separated integers.
+
+// Constraints
+// 1 < t < 100 1< n < 1000
+
+// Output Format
+// Print top k running stream.
+
+// Sample Input
+// 1
+// 5 2
+// 5 1 3 5 2
+// Sample Output
+// 5 1 5 1 3 5 1 5 1
