0ed7b17348259af99888f08b9c4da8ecaf31163d
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:03:58 +0530

    Update 9Kartik_Bhaiya_And_The_Celebrity_Problem.cpp

--- 24_Stacks/Challenges Stacks/9Kartik_Bhaiya_And_The_Celebrity_Problem.cpp
+++ 24_Stacks/Challenges Stacks/9Kartik_Bhaiya_And_The_Celebrity_Problem.cpp
@@ -63,3 +63,29 @@ int main(){
     }
     return 0;
 }
+
+
+// Kartik Bhaiya, mentor at Coding Blocks, organized a party for their interns at Coding Blocks. In a party of N people, only one person is known to everyone. Such a person may be present in the party, if yes, she/he doesn’t know anyone in the party. We can only ask questions like “does A know B? “.
+// Find the stranger (celebrity) in minimum number of questions.
+
+// Input Format
+// First line contains N, number of persons in party. Next line contains the matrix of N x N which represents A knows B when it's value is one.
+
+// Constraints
+// None
+
+// Output Format
+// Print the celebrity ID which is between 0 and N-1. If celebrity is not present then print "No Celebrity".
+
+// Sample Input
+// 4
+// 0 0 1 0
+// 0 0 1 0
+// 0 0 0 0
+// 0 0 1 0
+// Sample Output
+// 2
+// Explanation
+// In the given case there are 4 persons in the party let them as A, B, C, D. The matrix represents A knows B when it's value is one. From the matrix, A knows C, B knows C and D knows C. Thus C is the celebrity who doesnot know anyone and it's ID is 2. Hence output is 2.
+
+// Code Editor
