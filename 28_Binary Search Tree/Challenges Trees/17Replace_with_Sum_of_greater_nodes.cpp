adb662ecd001d1b7f27f6c0c3ef41b1698aebe19
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:26:38 +0530

    Update 17Replace_with_Sum_of_greater_nodes.cpp

--- 28_Binary Search Tree/Challenges Trees/17Replace_with_Sum_of_greater_nodes.cpp
+++ 28_Binary Search Tree/Challenges Trees/17Replace_with_Sum_of_greater_nodes.cpp
@@ -81,3 +81,54 @@ int main() {
 
     return 0;
 }
+
+
+
+// Given a binary search tree, replace each nodes' data with the sum of all nodes' which are greater or equal than it. Include the current node's data also.
+
+// Input Format
+// The first line contains a number n showing the length of the inorder array of BST. The next line contains n integers denoting the elements of the array.
+
+// Constraints
+// 2 ≤ N ≤ 10^3
+
+// Output Format
+// Print the preorder traversal of the new tree.
+
+// Sample Input
+// 7
+// 20 
+// 30 
+// 40 
+// 50 
+// 60 
+// 70 
+// 80
+// Sample Output
+// 260 330 350 300 150 210 80
+// Explanation
+// The original tree looks like
+
+//             50             
+//         /        \                
+//       30          70
+//     /    \       /   \ 
+//   20    40      60    80
+// We are supposed to replace the elements by the sum of elements larger than it.
+// 80 being the largest element remains unaffected .
+// 70 being the second largest element gets updated to 150 (70+80)
+// 60 becomes 210 (60 + 70 + 80)
+// 50 becomes 260 (50 + 60 + 70 + 80)
+// 40 becomes 300 (40 + 50 + 60 + 70 + 80)
+// 30 becomes 330 (30 + 40 + 50 + 60 + 70 + 80)
+// 20 becomes 350 (20 + 30 + 40 + 50 + 60 + 70 + 80)
+
+// The new tree looks like
+
+//            260             
+//         /        \                
+//      330         150
+//     /    \       /   \ 
+//   350    300   210    80
+// The Pre-Order traversal (Root->Left->Right) looks like :
+// 260 330 350 300 150 210 80.
