05737216067d6e17c299352741f2fe1ccaaf2b59
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:32:25 +0530

    Update 24BuildBST.cpp

--- 28_Binary Search Tree/Challenges Trees/24BuildBST.cpp
+++ 28_Binary Search Tree/Challenges Trees/24BuildBST.cpp
@@ -65,3 +65,33 @@ int main() {
     }
     return 0;
 }
+
+
+// You are given a sorted data of n integers. You have to form a balanced Binary Search Tree and then print preorder traversal of the tree.
+
+// Input Format
+// First line contains integer t as number of test cases.
+// Each test case contains following input. First line contains integer n which represents the length of the data and next line contains n space separated integers denoting the elements for the BST.
+
+// Constraints
+// 1 < t < 100
+// 1< n < 1000
+
+// Output Format
+// For each testcase , print the preorder traversal of the BST in a new line.
+
+// Sample Input
+// 1
+// 7
+// 1 2 3 4 5 6 7
+// Sample Output
+// 4 2 1 3 6 5 7
+// Explanation
+// The tree looks like
+
+//              4
+//           /      \
+//        2           6
+//     /     \      /    \
+//    1       3    5      7
+// We print the preorder of the final tree.
