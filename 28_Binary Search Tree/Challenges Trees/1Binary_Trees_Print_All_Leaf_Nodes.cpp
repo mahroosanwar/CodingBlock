70b74b7bc51f3ade798c886fe929c6fd8e70b945
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:12:07 +0530

    Update 1Binary_Trees_Print_All_Leaf_Nodes.cpp

--- 28_Binary Search Tree/Challenges Trees/1Binary_Trees_Print_All_Leaf_Nodes.cpp
+++ 28_Binary Search Tree/Challenges Trees/1Binary_Trees_Print_All_Leaf_Nodes.cpp
@@ -70,3 +70,28 @@ int main() {
 
     return 0;
 }
+
+
+
+// Given a Binary Tree Print all the leaf nodes.
+
+// Input Format
+// Level order input of the binary tree
+
+// Constraints
+// Total no of nodes <1000
+
+// Output Format
+// All leaf nodes from left to right in single line
+
+// Sample Input
+// 1
+// 2 3
+// 4 5
+// 6 7
+// -1 -1
+// -1 -1
+// -1 -1
+// -1 -1
+// Sample Output
+// 4 5 6 7
