cd5961bed1225d855ba2cb4e0b6af3a44c54ecf7
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:25:56 +0530

    Update 16Print_all_nodes_at_distance_k_from_a_given_node.cpp

--- 28_Binary Search Tree/Challenges Trees/16Print_all_nodes_at_distance_k_from_a_given_node.cpp
+++ 28_Binary Search Tree/Challenges Trees/16Print_all_nodes_at_distance_k_from_a_given_node.cpp
@@ -176,3 +176,36 @@ int main() {
 
     return 0;
 }
+
+
+// Given a binary tree, a target node in the binary tree, and an integer value k, print all the nodes that are at distance k from the given target node. No parent pointers are available. Print 0 if no such node exist at distance k.
+
+// Input Format
+// The first line of input will contain an integer n. The next line will contain n integers denoting the the preorder traversal of the BT. The next line will contain n more integers denoting the inorder traversal of the BT. The next line will contain an integer T. Then T lines follow you will be given 2 integers the first one denoting the value of Node and the second one denoting the value of k.
+
+// Constraints
+// 2 ≤ N ≤ 10^3
+
+// Output Format
+// On T lines print space separated desired output for each test case in sorted form
+
+// Sample Input
+// 4
+// 60 65 50 70
+// 50 65 60 70
+// 2
+// 60 1
+// 65 2
+// Sample Output
+// 65 70
+// 70
+// Explanation
+// The tree looks like
+
+//              60
+//           /      \
+//        65         70
+//      /
+//   50
+// For testcase 1 : The nodes 65 and 70 are the only ones at distance k=1 from 60.
+// For testcase 2 : The node 70 is at distance k=2 from 65.
