1b2d9145096732e467cf0d45d193cd084299414d
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 18:30:21 +0530

    Update 7StringWindow1.cpp

--- 30_Hashing - Hashtable/Challenges Hashing/7StringWindow1.cpp
+++ 30_Hashing - Hashtable/Challenges Hashing/7StringWindow1.cpp
@@ -56,3 +56,23 @@ int main()
     cout<<window(s,p)<<endl;
     return 0;
 }
+
+
+// Ravi has been given two strings named string1 and string 2. He is supposed to find the minimum length substring of the string1 which contains all the characters of string2. Help him to find the substring
+
+// Input Format
+// The first line of the input contains string1. String1 can be a string containing spaces also. The second line of the input contains string2. String2 can be a string containing spaces also.
+
+// Constraints
+// Length of both the strings can be at max 10^4
+
+// Output Format
+// Output the substring in a single line. If no such substring exist then output "No String" without quotes
+
+// Sample Input
+// qwerty asdfgh qazxsw
+// qas
+// Sample Output
+// qazxs
+// Explanation
+// The substring "qazxs" is the shortest substring of s1 that contains all the characters of s2.
