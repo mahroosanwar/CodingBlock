efe2712776d0acc610b2e4caf9b51e6e884d2c4e
Mahroos Anwar <81352498+mahroosanwar@users.noreply.github.com>
2022-08-03 10:02:26 +0530

    Update 7Balanced_Parenthesis.cpp

--- 24_Stacks/Challenges Stacks/7Balanced_Parenthesis.cpp
+++ 24_Stacks/Challenges Stacks/7Balanced_Parenthesis.cpp
@@ -57,3 +57,23 @@ int main(){
 
     return 0;
 }
+
+
+// You are given a string of brackets i.e. '{', '}' , '(' , ')', '[' , ']' . You have to check whether the sequence of parenthesis is balanced or not.
+// For example, "(())", "(())()" are balanced and "())(", "(()))" are not.
+
+// Input Format
+// A string of '(' , ')' , '{' , '}' and '[' , ']' .
+
+// Constraints
+// 1<=|S|<=10^5
+
+// Output Format
+// Print "Yes" if the brackets are balanced and "No" if not balanced.
+
+// Sample Input
+// (())
+// Sample Output
+// Yes
+// Explanation
+// (()) is a balanced string.
